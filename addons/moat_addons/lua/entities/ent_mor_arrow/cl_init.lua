67c384b0bc1b7eab324bcc9b48574cde7e740e20
moat7 <cole92107@yahoo.com>
2018-03-03 15:32:45 -0800

    Make arrow trail a little better.

--- addons/moat_addons/lua/entities/ent_mor_arrow/cl_init.lua
+++ addons/moat_addons/lua/entities/ent_mor_arrow/cl_init.lua
@@ -4,12 +4,11 @@ include('shared.lua')
 local trail = Material("trails/plasma")
 
 function ENT:UpdateSavedPos()
-	self.savedpos = self:GetPos()
-	self.nextposcheck = CurTime() + 0.05
+	self.lastpos = self:GetPos()
 end
 
 function ENT:Think()
-	if (not self.nextposcheck or (self.nextposcheck <= CurTime())) then self:UpdateSavedPos() end
+	if (not self.lastpos or (self:GetPos():DistToSqr(self.lastpos) > 3)) then self:UpdateSavedPos() end
 end
 
 function ENT:Draw()
@@ -20,8 +19,8 @@ function ENT:Draw()
 
     if (not IsValid(self:GetOwner())) then return end
     if (self:GetOwner() ~= LocalPlayer()) then return end
-    if (not self.savedpos) then return end
+    if (not self.lastpos) then return end
 
     render.SetMaterial(trail)
-    render.DrawBeam(self:GetPos(), self.savedpos, 5, 1, 1, Color(255, 0, 0, 255))
+    render.DrawBeam(self:GetPos(), self.lastpos, 5, 1, 1, Color(255, 0, 0, 255))
 end
