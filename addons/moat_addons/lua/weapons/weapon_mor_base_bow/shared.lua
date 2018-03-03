0e6dd70c27993bea9fe3570e9d252c3b8da4727d
moat <moat7@users.noreply.github.com>
2018-03-03 15:40:39 -0800

    Bow Prediction + Arrow Updates (#37)
    * make bow code standardized more and zoom prediction
    
    * Make bows able to be used by two people at a time - more prediction
    
    * rework bow networking - change maxholdtime to 2 seconds, change deployment to allow instant pullback, made code p r e t t y
    
    * bow arrows are now predicted
    
    * fix predicted hud on bow
    
    * Make arrow trail only visible to bow owner.
    
    * fix error
    
    * fix infinite bounce on bow arrows
    
    * update daedric bow, decrease velocity of arrows
    
    * increase velocity of arrows a bit more, was too slow
    
    * Make arrow trail a little better.

--- addons/moat_addons/lua/weapons/weapon_mor_base_bow/shared.lua
+++ addons/moat_addons/lua/weapons/weapon_mor_base_bow/shared.lua
@@ -8,51 +8,51 @@ if( CLIENT ) then
 	SWEP.Slot = 2
 	SWEP.DrawAmmo = true
 	SWEP.DrawCrosshair = false
-   SWEP.Icon = "vgui/ttt/icon_scout"
+	SWEP.Icon = "vgui/ttt/icon_scout"
 
-   local grad_d = Material("vgui/gradient-d")
+	local grad_d = Material("vgui/gradient-d")
 
-   function SWEP:DrawHUD()
-      local client = LocalPlayer()
-      if (not IsValid(client)) then return end
+	function SWEP:DrawHUD()
+		local client = LocalPlayer()
+		if (not IsValid(client)) then return end
 
-      local sights = (not self.NoSights) and self:GetIronsights()
+		local sights = (not self.NoSights) and self:GetIronsights()
 
-      local x = math.floor(ScrW() / 2.0)
-      local y = math.floor(ScrH() / 2.0)
-      local scale = math.max(0.2,  10 * self:GetPrimaryCone())
+		local x = math.floor(ScrW() / 2.0)
+		local y = math.floor(ScrH() / 2.0)
+		local scale = math.max(0.2,  10 * self:GetPrimaryCone())
 
-      local LastShootTime = self:LastShootTime()
-      scale = scale * (2 - math.Clamp( (CurTime() - LastShootTime) * 5, 0.0, 1.0 ))
+		local LastShootTime = self:LastShootTime()
+		scale = scale * (2 - math.Clamp( (CurTime() - LastShootTime) * 5, 0.0, 1.0 ))
 
-      local alpha = sights and 0.8 or 1
-      local bright = 1
+		local alpha = sights and 0.8 or 1
+		local bright = 1
 
-      if client.IsTraitor and client:IsTraitor() then
-         surface.SetDrawColor(255 * bright,
-                              50 * bright,
-                              50 * bright,
-                              255 * alpha)
-      else
-         surface.SetDrawColor(0,
-                              255 * bright,
-                              0,
-                              255 * alpha)
-      end
+		if client.IsTraitor and client:IsTraitor() then
+			surface.SetDrawColor(255 * bright,
+								50 * bright,
+								50 * bright,
+								255 * alpha)
+		else
+			surface.SetDrawColor(0,
+								255 * bright,
+								0,
+								255 * alpha)
+		end
 
-      local gap = math.floor(20 * scale * (sights and 0.8 or 1))
-      local length = math.floor(gap + (25 * 1) * scale)
+		local gap = math.floor(20 * scale * (sights and 0.8 or 1))
+		local length = math.floor(gap + (25 * 1) * scale)
 
-      surface.DrawLine(x - 6, y + 6, x, y)
-      surface.DrawLine(x + 6, y + 6, x, y)
+		surface.DrawLine(x - 6, y + 6, x, y)
+		surface.DrawLine(x + 6, y + 6, x, y)
 
-      surface.DrawLine(x - 6, y + 5, x, y - 1)
-      surface.DrawLine(x + 6, y + 5, x, y - 1)
+		surface.DrawLine(x - 6, y + 5, x, y - 1)
+		surface.DrawLine(x + 6, y + 5, x, y - 1)
 
-      surface.DrawLine(x - 5, y + 15, x + 5, y + 15)
-      surface.DrawLine(x - 4, y + 25, x + 4, y + 25)
-      surface.DrawLine(x - 3, y + 35, x + 3, y + 35)
-      surface.DrawLine(x - 2, y + 45, x + 2, y + 45)
+		surface.DrawLine(x - 5, y + 15, x + 5, y + 15)
+		surface.DrawLine(x - 4, y + 25, x + 4, y + 25)
+		surface.DrawLine(x - 3, y + 35, x + 3, y + 35)
+		surface.DrawLine(x - 2, y + 45, x + 2, y + 45)
 
 
 		local scrx = ScrW()/2
@@ -60,9 +60,8 @@ if( CLIENT ) then
 		surface.SetDrawColor(255, 255, 255, 100)
 		--surface.DrawRect(scrx - 75, scry + 200, 150, 16)
 		surface.DrawOutlinedRect(scrx - 75, scry + 200, 150, 16)
-		if self:GetDTFloat(0) != 0 then
-			local ratio = math.Clamp((self.MaxHoldTime + CurTime() - self:GetDTFloat(0))/(self.MaxHoldTime * .5), .5, 1)
-			ratio = 2 * (ratio - .5)
+		if self.UnpredictedHoldTime and self.UnpredictedHoldTime ~= 0 then
+			local ratio = (math.Clamp((SysTime() - self.UnpredictedHoldTime) / self.MaxHoldTime, 0.1, 1) - 0.1) / 0.9
 			surface.SetDrawColor(255, 0, 0, 180)
 			surface.DrawRect(scrx - 75, scry + 200, 150 * ratio, 16)
 
@@ -72,7 +71,7 @@ if( CLIENT ) then
 		end
 
 		draw.SimpleText("Bow Power", "ChatFont", scrx, scry + 207, Color(255, 255, 255), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER)
-    end
+	end
 end
 
 SWEP.PrintName = "Morrowind Bow"
@@ -109,10 +108,7 @@ SWEP.Secondary.Automatic	= false				// Automatic/Semi Auto
 SWEP.Secondary.Ammo		= "none"
 SWEP.HoldType     = "Pistol"
 SWEP.QuickFireRatio		= .6			// Changes quickfire damage/velocity, a ratio of normal damage/velocity.
-SWEP.MaxHoldTime		= 3				// Must let go after this time. Determines damage/velocity based on time held.
-//Do not touch.
-//SWEP.FiredArrow			= false
-SWEP.DelayTime			= 0
+SWEP.MaxHoldTime		= 2				// Must let go after this time. Determines damage/velocity based on time held.
 
 function SWEP:Precache()
 	util.PrecacheSound("sound/weapons/bow/skyrim_bow_draw.mp3")
@@ -121,37 +117,39 @@ function SWEP:Precache()
 end
 
 function SWEP:Initialize()
-    self:SetWeaponHoldType("Pistol")
+	self:SetWeaponHoldType("Pistol")
+	self:SetZoomed(false)
+	self:CreateArrow()
 end
-/*
-function SWEP:Equip()
-	self:Deploy()
-end
-*/
+
 function SWEP:SetupDataTables()
-	self:DTVar("Bool", 0, "FiredArrow")
-	self:DTVar("Float", 0, "HoldTimer")
+	self:NetworkVar("Float", 0, "HoldTime")
+	self:NetworkVar("Float", 1, "AnimationResetTime")
+	self:NetworkVar("Bool", 1, "Zoomed")
+	self:NetworkVar("Entity", 0, "Arrow")
+end
+
+function SWEP:ResetNetworkable()
+	self:SetHoldTime(0)
+	self.UnpredictedHoldTime = 0
+	self:SetAnimationResetTime(0)
+	self:SetZoomed(false)
 end
 
 function SWEP:Deploy()
 	self:EmitSound("weapons/bow/skyrim_bow_draw.mp3")
-	self.Weapon:SetNextPrimaryFire(CurTime() + 1)
-	self.Weapon:SetNextSecondaryFire(CurTime() + 1)	
-	self.DelayTime = CurTime() + 1
-	if (SERVER) then
-		local vm = self.Owner:GetViewModel()
-		if (vm:IsValid()) then
-			vm:SetPlaybackRate(1)
-		end
+	self:SetNextPrimaryFire(CurTime())
+	local vm = self.Owner:GetViewModel()
+	if (vm:IsValid()) then
+		vm:SetPlaybackRate(1)
 	end
-	self.Weapon:SendWeaponAnim(ACT_VM_DRAW)
-	self:SetDTBool(0, false)
-	self:SetDTFloat(0, 0)
+	self:SendWeaponAnim(ACT_VM_DRAW)
+	self:ResetNetworkable()
 	return true
 end
 
 function SWEP:Holster()
-    self.Zoomed = false
+	self:SetZoomed(false)
 	if SERVER then
 		self.Owner:SetFOV( 0, 0.3 ) -- Setting to 0 resets the FOV
 	end
@@ -163,43 +161,32 @@ end
    Desc: +attack1 has been pressed.
 ---------------------------------------------------------*/
 function SWEP:PrimaryAttack()
-	if self:GetDTFloat(0) != 0 then return end
-    if (not self:CanPrimaryAttack()) then return end
+	if (self:GetHoldTime() ~= 0) then return end
+	if (not self:CanPrimaryAttack()) then return end
 
-    self:SetNextPrimaryFire(self:GetDTFloat(0))
+	self:SendWeaponAnim(ACT_VM_RELOAD)
 
-	if (SERVER) then
-		local vm = self.Owner:GetViewModel()
-		if (vm:IsValid()) then
-			vm:SetPlaybackRate(1)
-		end
+	local vm = self.Owner:GetViewModel()
+	if (vm:IsValid()) then
+		vm:SetPlaybackRate(1)
 	end
 
-	local anim = self.Owner:GetViewModel():LookupSequence("reload"..math.random(1,3))
-	self.Weapon:SendWeaponAnim(ACT_VM_RELOAD)
-
-	if (SERVER) then
-		local vm = self.Owner:GetViewModel()
-		if (vm:IsValid()) then
-			vm:SetPlaybackRate(1)
-		end
+	self:SetHoldTime(CurTime())
+	if (IsFirstTimePredicted()) then
+		self.UnpredictedHoldTime = SysTime()
 	end
 
-	self.Weapon:EmitSound("weapons/bow/skyrim_bow_pull.mp3")
-	self.DelayTime = CurTime() + 0.5
-	self:SetDTFloat(0, CurTime() + self.MaxHoldTime)
-	self:SetNextPrimaryFire(self:GetDTFloat(0))
+	self:EmitSound("weapons/bow/skyrim_bow_pull.mp3")
 end
 
 function SWEP:CanPrimaryAttack()
-   if not IsValid(self.Owner) then return end
-   --if (self.DelayTime > CurTime()) then return end
-   
-   if self:Clip1() <= 0 then
-      self:DryFire(self.SetNextPrimaryFire)
-      return false
-   end
-   return true
+	if not IsValid(self.Owner) then return end
+
+	if self:Clip1() <= 0 then
+		self:DryFire(self.SetNextPrimaryFire)
+		return false
+	end
+	return true
 end
 
 /*---------------------------------------------------------
@@ -209,21 +196,34 @@ end
 function SWEP:Think()
 	if !self.Owner:IsValid() or !self.Owner:IsPlayer() then return end
 
-	if self:GetDTFloat(0) != 0 and self.DelayTime < CurTime() and self:GetDTBool(0) == false then
-		if !self.Owner:KeyDown(IN_ATTACK) then
+	if self:GetHoldTime() ~= 0 then
+		if (!self.Owner:KeyDown(IN_ATTACK)) then
+			local vm = self.Owner:GetViewModel()
+			if (vm:IsValid()) then
+				vm:SetPlaybackRate(1)
+			end
+			self:ShootArrow()
 			if SERVER then
-				local vm = self.Owner:GetViewModel()
-				if (vm:IsValid()) then
-					vm:SetPlaybackRate(1)
-				end
-				self:ShootArrow()
 				Damagelog:shootCallback(self)
 			end
-			self:SetDTBool(0, true)
-			self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
-			self:SetNextSecondaryFire(CurTime() + self.Primary.Delay)
 		end
+		self:SetNextPrimaryFire(CurTime() + self.Primary.Delay)
 	end
+
+	if (self:GetAnimationResetTime() ~= 0 and self:GetAnimationResetTime() < CurTime()) then
+		self:SetAnimationResetTime(0)
+		local anim = self.Owner:GetViewModel():LookupSequence("idle")
+		self.Owner:GetViewModel():ResetSequence(anim)
+		self.Owner:GetViewModel():SetPlaybackRate(1)
+//		self.Owner:GetViewModel():SetCycle(10)
+	end
+
+end
+
+function SWEP:CreateArrow()
+	if (not SERVER) then return end
+	self:SetArrow(ents.Create "ent_mor_arrow")
+	self:GetArrow():SetRenderMode(RENDERMODE_NONE)
 end
 
 /*---------------------------------------------------------
@@ -231,50 +231,39 @@ end
    Desc: Hot Potato.
 ---------------------------------------------------------*/
 function SWEP:ShootArrow()
-	if self.DelayTime > CurTime() then return end
 	local anim = self.Owner:GetViewModel():LookupSequence("shoot"..math.random(4,6))
 	self.Owner:GetViewModel():SetSequence(anim)
 	self.Owner:GetViewModel():SetPlaybackRate(4)
-	timer.Simple(.2, function()
-		if not self.Owner or not self.Owner:IsValid() then return end
-		local anim = self.Owner:GetViewModel():LookupSequence("idle")
-		self.Owner:GetViewModel():ResetSequence(anim)
-		self.Owner:GetViewModel():SetPlaybackRate(1)
-//		self.Owner:GetViewModel():SetCycle(10)
-	end)
-	timer.Simple(.2, function()
-		if !self.Owner then return end
-		local ratio = math.Clamp((self.MaxHoldTime + CurTime() - self:GetDTFloat(0))/(self.MaxHoldTime * .5), .5, 1)
-		self.Weapon:EmitSound("weapons/bow/skyrim_bow_shoot.mp3")
-		
-		local arrow = ents.Create("ent_mor_arrow")
-		if SERVER then
-			arrow:SetAngles(self.Owner:EyeAngles())
-			local pos = self.Owner:GetShootPos()
-				pos = pos + self.Owner:GetUp() * -3
-			arrow:SetPos(pos)
-			arrow:SetOwner(self.Owner)
-			arrow.Weapon = self		// Used to set the arrow's killicon.
-			arrow.Damage = (self.Primary.Damage * ratio)
-			arrow:SetAngles(self.Owner:EyeAngles() + Angle(0, 180, 0))
-			local trail = util.SpriteTrail(arrow, 0, Color(255, 255, 255, 100), false, 5, 5, 0.05, 1/(15+1)*0.5, "trails/plasma.vmt")
-			arrow.Trail = trail
-			arrow:Spawn()
-			arrow:Activate()
-		end
-		//		self.Weapon:SendWeaponAnim(ACT_VM_IDLE)
-		self.Owner:SetAnimation(PLAYER_ATTACK1)
-		self:SetDTFloat(0, 0)
-		self:SetDTBool(0, false)
-		if SERVER then
-			local phys = arrow:GetPhysicsObject()
-			phys:SetVelocity(self.Owner:GetAimVector() * 5000 * ratio)
-			arrow:SetOwner(self.Owner)
-		end
-		self:SetNextPrimaryFire(CurTime())
-		self:SetNextSecondaryFire(CurTime())
-		self:TakePrimaryAmmo(1)
-	end)
+	self:SetAnimationResetTime(CurTime() + 0.2)
+	self:SetNextPrimaryFire(CurTime() + 1)
+	self:SetHoldTime(0)
+	if (IsFirstTimePredicted()) then
+		self.UnpredictedHoldTime = 0
+	end
+
+	local ratio = math.Clamp((CurTime() - self:GetHoldTime()) / self.MaxHoldTime, 0.1, 1)
+	self:EmitSound("weapons/bow/skyrim_bow_shoot.mp3")
+
+	local arrow = self:GetArrow()
+	arrow:SetRenderMode(RENDERMODE_NORMAL)
+	arrow:SetModel("models/morrowind/steel/arrow/steelarrow.mdl")
+	self:CreateArrow()
+	arrow:SetAngles(self.Owner:EyeAngles())
+	local pos = self.Owner:GetShootPos()
+	pos = pos + self.Owner:GetUp() * -3
+	arrow:SetPos(pos)
+	arrow:SetOwner(self.Owner)
+	arrow.Weapon = self		-- Used to set the arrow's killicon.
+	arrow.Damage = self.Primary.Damage * ratio
+	arrow:SetAngles(self.Owner:EyeAngles() + Angle(0, 180, 0))
+	arrow:Spawn()
+	arrow:Activate()
+	//		self.Weapon:SendWeaponAnim(ACT_VM_IDLE)
+	self.Owner:SetAnimation(PLAYER_ATTACK1)
+	arrow.Velocity = self.Owner:GetAimVector() * 3500 * ratio
+	arrow:SetFirer(self.Owner)
+	self:SetNextPrimaryFire(CurTime())
+	self:TakePrimaryAmmo(1)
 end
 
 /*---------------------------------------------------------
@@ -282,26 +271,20 @@ end
    Desc: +attack1 has been pressed.
 ---------------------------------------------------------*/
 function SWEP:SecondaryAttack()
-	 if !self.Zoomed then -- The player is not zoomed in.
-		self.Zoomed = true -- Now he is
-		if SERVER then
-			self.Owner:SetFOV( 35, 0.3 ) -- SetFOV is serverside only
-		end
+	 if not self:GetZoomed() then -- The player is not zoomed in.
+		self:SetZoomed(true)
+		self.Owner:SetFOV( 35, 0.3 )
 	else
-		self.Zoomed = false
-		if SERVER then
-			self.Owner:SetFOV( 0, 0.3 ) -- Setting to 0 resets the FOV
-		end
+		self:SetZoomed(false)
+		self.Owner:SetFOV( 0, 0.3 ) -- Setting to 0 resets the FOV
 	end
 end
 
 function SWEP:Reload()
 	if (self:Clip1() >= self.Primary.ClipSize or self.Owner:GetAmmoCount( self.Primary.Ammo ) <= 0) then return end
-   self:DefaultReload(0)
-   self.Zoomed = false
-	if SERVER then
-		self.Owner:SetFOV( 0, 0.3 ) -- Setting to 0 resets the FOV
-	end
+	self:DefaultReload(0)
+	self:SetZoomed(false)
+	self.Owner:SetFOV( 0, 0.3 ) -- Setting to 0 resets the FOV
 end
 
 function MorBowArrow(ent, ragdoll)
