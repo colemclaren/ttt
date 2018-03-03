a10f7c347b0fedabf6ebb9b9309eab0bc49f8fed
moat7 <cole92107@yahoo.com>
2018-03-03 12:53:36 -0800

    Fixed the following talents from being exploitable during boss rounds: Adrenaline Rush, Speedforce, Meticulous, and Sustained.

--- addons/moat_addons/lua/moat_items/moat_talents/meticulous.lua
+++ addons/moat_addons/lua/moat_items/moat_talents/meticulous.lua
@@ -20,7 +20,7 @@ TALENT.Melee = false
 TALENT.NotUnique = true
 
 function TALENT:OnPlayerHit(victim, attacker, dmginfo, talent_mods)
-	if (victim:Health() - dmginfo:GetDamage() <= 0) then
+	if (GetRoundState() == ROUND_ACTIVE and not MOAT_ACTIVE_BOSS and (victim:Health() - dmginfo:GetDamage() <= 0)) then
 		local chanceNum = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
 		local randomNum = math.Rand(1, 100)
 		local applyMod = chanceNum > randomNum
