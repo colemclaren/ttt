bbd44a75a78c2d4d473062d0934d8354a935c5a7
moat7 <cole92107@yahoo.com>
2018-03-09 03:27:40 -0800

    slipped again -_-

--- gamemodes/terrorcity/gamemode/init.lua
+++ gamemodes/terrorcity/gamemode/init.lua
@@ -891,7 +891,7 @@ function SelectRoles()
       if (random_roles and #random_roles < 1) then r(pl, ROLE_INNOCENT) continue end
 
       local role_rand = math.random(1, #random_roles)
-      if (roles[role_rand] < 1) then r(pl, roles[role_rand]) random_roles[role_rand] = nil continue end
+      if (roles[random_roles[role_rand]] < 1) then r(pl, random_roles[role_rand]) random_roles[role_rand] = nil continue end
     end
 
     for _, ply in ipairs(pls) do
