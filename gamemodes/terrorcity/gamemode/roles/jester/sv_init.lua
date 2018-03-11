1209ed70adbb9ee78dc775cf32ead61fb1d15b83
Meepen <meepdarknessmeep@gmail.com>
2018-03-11 12:27:30 -0400

    Revert "Revert "change jester - jester now makes time go by 5x as fast and no winners = jester win""

--- gamemodes/terrorcity/gamemode/roles/jester/sv_init.lua
+++ gamemodes/terrorcity/gamemode/roles/jester/sv_init.lua
@@ -4,12 +4,29 @@ function ROLE:PlayerShouldTakeDamage(ply)
     return false
 end
 
+InstallRoleHook("PlayerDeath", 1)
 util.AddNetworkString "jester.killed"
 
-hook.Add("PlayerDeath", "jester.killer", function(pl, inf, att)
-    if (GetRoundState() == ROUND_ACTIVE and pl:GetRole() == ROLE_JESTER and IsValid(att) and att:IsPlayer() and pl ~= att) then
+local ded = false
+
+function ROLE:PlayerDeath(pl, inf, att)
+    if (GetRoundState() == ROUND_ACTIVE and IsValid(att) and att:IsPlayer() and pl ~= att and att:GetBasicRole() == ROLE_INNOCENT) then
         net.Start("jester.killed")
         net.WriteString(att:Nick() or "Someone")
         net.Broadcast()
+        StartRoundSpeedup(5)
+        ded = true
     end
+end
+
+hook.Add("TTTBeginRound", "terrorcity.roles.jester", function()
+    ded = false
 end)
+
+hook.Add("TTTEndRound", "terrorcity.roles.jester", function()
+    StartRoundSpeedup(1)
+end)
+
+function DidJesterDie()
+    return ded
+end
