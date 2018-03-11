b7576271d7a9588cbe9de009e89c7bea137b8035
Meepen <meepdarknessmeep@gmail.com>
2018-03-11 18:03:47 -0400

    change hitman hud to show below targetid

--- gamemodes/terrorcity/gamemode/roles/hitman/cl_init.lua
+++ gamemodes/terrorcity/gamemode/roles/hitman/cl_init.lua
@@ -7,13 +7,13 @@ end)
 function ROLE.HUDPaint()
     if (IsValid(ROLE.Target) and LocalPlayer():GetRole() == ROLE_HITMAN) then
         local name = ROLE.Target:Nick()
-        surface.SetFont "TimeLeft" -- better font pls
+        surface.SetFont "TargetID" -- better font pls
         local w = surface.GetTextSize "Target: "
         local w2 = surface.GetTextSize(name)
         local tw = w + w2
-        local centerx, centery = ScrW() / 2, ScrH() / 2
-        draw.SimpleTextOutlined("Target: ", "TimeLeft", centerx - tw / 2, centery + 50, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 2, Color(0, 0, 0, 35))
-        draw.SimpleTextOutlined(name, "TimeLeft", centerx - tw / 2 + w, centery + 50, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 2, Color(0, 0, 0, 35))
+        local centerx, centery = ScrW() / 2, ScrH() / 2 + 90
+        draw.SimpleTextOutlined("Target: ", "TimeLeft", centerx - tw / 2, centery, color_white, TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 2, Color(0, 0, 0, 35))
+        draw.SimpleTextOutlined(name, "TimeLeft", centerx - tw / 2 + w, centery, Color(230, 20, 20), TEXT_ALIGN_LEFT, TEXT_ALIGN_CENTER, 2, Color(0, 0, 0, 35))
     end
 end
 
