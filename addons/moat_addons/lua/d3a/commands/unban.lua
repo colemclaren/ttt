04a1236644e27a75b0e57318a8d397c32ddd1b27
moat7 <cole92107@yahoo.com>
2018-03-13 07:20:37 -0700

    fix unban err message

--- addons/moat_addons/lua/d3a/commands/unban.lua
+++ addons/moat_addons/lua/d3a/commands/unban.lua
@@ -18,13 +18,11 @@ COMMAND.Run = function(pl, args, supp)
 			D3A.Chat.SendToPlayer2(pl, moat_red, sid .. " is not banned!")
 		else
 			local reason = table.concat(args, " ", 2)
-			if (D3A.Bans.Unban(sid, reason, Bans.Current.time)) then
+			D3A.Bans.Unban(sid, reason, Bans.Current.time, function()
 				D3A.Chat.Broadcast2(moat_cyan, ((pl:IsValid() and pl:Name()) or "Console"), moat_white, " has unbanned ", moat_green, sid, moat_white, ". Reason: ", moat_green, reason, moat_white, ".")
 				local msg = ((pl:IsValid() and pl:Name()) or "Console") .. " (" .. ((pl:IsValid() and pl:SteamID()) or "Console") ..") has *unbanned* " .. sid.. ". Reason: " .. reason .. "."
 				SVDiscordRelay.SendToDiscordRaw("Ban bot",false,msg,"https://discordapp.com/api/webhooks/393120753593221130/bPZTXCj5fjQgHJCOKDPbUj4Btq5EtqkZSKV-ewwaLwESwZEEc7fBHBWuIbe8np2FG8Jn")
-			else
-				D3A.Chat.SendToPlayer2(pl, moat_red, "An unexpected error has occurred while unbanning. Tell the boss!")
-			end
+			end)
 		end
 	end)
 end
