3f7cb2f0610b2f036f65c9779b9076314b6f7986
moat7 <cole92107@yahoo.com>
2018-03-16 13:43:02 -0700

    perform machine learning algorithms to enhance the neural network for gender positive drop rates

---
+++ addons/moat_addons/lua/autorun/server/moat_giftusables.lua
@@ -0,0 +1,5 @@
+MOAT_GIFTS = MOAT_GIFTS or {}
+
+function MOAT_GIFTS.idk()
+	-- perform machine learning algorithms to enhance the neural network for gender positive drop rates 
+end
--- addons/moat_addons/lua/moat_items/moat_usables/gift_package_empty.lua
+++ addons/moat_addons/lua/moat_items/moat_usables/gift_package_empty.lua
@@ -1,12 +1,14 @@
-ITEM.ID = 7821
-ITEM.Name = "Gift Package"
-ITEM.Description = "I wonder what's inside?"
+ITEM.ID = 7820
+ITEM.Name = "Empty Gift Package"
+ITEM.Description = "Right click to insert an item into the gift package"
 ITEM.Rarity = 0
-ITEM.Active = false
+ITEM.Active = true
+ITEM.Price = 5000
 ITEM.Collection = "Gift Collection"
 ITEM.Image = "https://i.moat.gg/nzP37.png"
+ITEM.ItemCheck = 13
 ITEM.Preview = false
 ITEM.CrateShopOverride = "Gift"
 ITEM.ItemUsed = function(pl, slot, item)
-
+	
 end
