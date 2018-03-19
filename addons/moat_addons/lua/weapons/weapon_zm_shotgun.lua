6b3bbde480c7cf574da80e81ac4c6b7161da3e03
Meepen <meepdarknessmeep@gmail.com>
2018-03-19 02:55:00 -0400

    revert xm1014 spread and change delay on shotguns again

--- addons/moat_addons/lua/weapons/weapon_zm_shotgun.lua
+++ addons/moat_addons/lua/weapons/weapon_zm_shotgun.lua
@@ -21,14 +21,14 @@ SWEP.WeaponID = AMMO_SHOTGUN
 SWEP.ENUM = 2
 
 SWEP.Primary.Ammo = "Buckshot"
-SWEP.Primary.Damage = 10
-SWEP.Primary.Cone = 0.2
-SWEP.Primary.Delay = 0.8 + 0.5
+SWEP.Primary.Damage = 7
+SWEP.Primary.Cone = 0.12
+SWEP.Primary.Delay = 0.8 + 0.2
 SWEP.Primary.ClipSize = 8
 SWEP.Primary.ClipMax = 24
 SWEP.Primary.DefaultClip = 8
 SWEP.Primary.Automatic = true
-SWEP.Primary.NumShots = 8
+SWEP.Primary.NumShots = 11
 SWEP.AutoSpawnable      = true
 SWEP.AmmoEnt = "item_box_buckshot_ttt"
 
