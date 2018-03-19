29c2b9940718f32310e0a100c4db90f028d9e3b7
moat7 <cole92107@yahoo.com>
2018-03-19 12:21:47 -0700

    increase clip size of some bows

--- addons/moat_addons/lua/weapons/weapon_mor_bonemold/shared.lua
+++ addons/moat_addons/lua/weapons/weapon_mor_bonemold/shared.lua
@@ -20,9 +20,9 @@ SWEP.WorldModel   = "models/morrowind/bonemold/longbow/w_bonemold_longbow.mdl"
 SWEP.Primary.Damage		= 100
 SWEP.Primary.Velocity 		= 3000
 
-SWEP.Primary.ClipSize = 8
-SWEP.Primary.ClipMax = 16 -- keep mirrored to ammo
-SWEP.Primary.DefaultClip = 8
+SWEP.Primary.ClipSize = 10
+SWEP.Primary.ClipMax = 30 -- keep mirrored to ammo
+SWEP.Primary.DefaultClip = 10
 SWEP.Primary.Automatic		= true				// Automatic/Semi Auto
 SWEP.Primary.Ammo			= "357"
 SWEP.Crosshair				= true
