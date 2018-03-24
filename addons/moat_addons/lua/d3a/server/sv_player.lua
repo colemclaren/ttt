f9301170228ca8d5eba151b251f31574605114e4
Meepen <meepdarknessmeep@gmail.com>
2018-03-24 09:07:53 -0400

    fix query error

--- addons/moat_addons/lua/d3a/server/sv_player.lua
+++ addons/moat_addons/lua/d3a/server/sv_player.lua
@@ -5,8 +5,8 @@ function D3A.Player.CanTarget(pl1, pl2)
 end
 
 function D3A.Player.InsertNewPlayerToTable(SteamID, SteamID32, IP, Name, AvatarURL)
-	D3A.MySQL.Query("INSERT INTO player (`steam_id`, `name`, `first_join`, `avatar_url`, `inventory_credits`, `event_credits`, `donator_credits`, `extra`) VALUES ('" .. SteamID .."', '" .. D3A.MySQL.Escape(Name) .. "', '" .. os.time() .. "', '" .. AvatarURL .. "', 0, 0, 0, null);")
-	D3A.MySQL.Query("INSERT INTO player_iplog (`SteamID`, `Address`, `LastSeen`) VALUES ('" .. SteamID32 .. "', '" .. IP .. "', '-1');")
+	D3A.MySQL.Query("REPLACE INTO player (`steam_id`, `name`, `first_join`, `avatar_url`, `inventory_credits`, `event_credits`, `donator_credits`, `extra`) VALUES ('" .. SteamID .."', '" .. D3A.MySQL.Escape(Name) .. "', '" .. os.time() .. "', '" .. AvatarURL .. "', 0, 0, 0, null);")
+	D3A.MySQL.Query("REPLACE INTO player_iplog (`SteamID`, `Address`, `LastSeen`) VALUES ('" .. SteamID32 .. "', '" .. IP .. "', '-1');")
 	D3A.Print(SteamID32 .. " | Connecting for the first time")
 end
 
