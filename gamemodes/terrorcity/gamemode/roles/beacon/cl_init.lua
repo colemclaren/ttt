15a86dcc2d9c5a426f0535317432ea66e41b6492
Meepen <meepdarknessmeep@gmail.com>
2018-03-26 18:55:36 -0400

    beacon lightning

--- gamemodes/terrorcity/gamemode/roles/beacon/cl_init.lua
+++ gamemodes/terrorcity/gamemode/roles/beacon/cl_init.lua
@@ -2,6 +2,4 @@ net.Receive("tc_beacon_pos", function(len, ply)
     local data = EffectData()
     data:SetOrigin(net.ReadVector())
     util.Effect("beacon_lightning", data)
-    print "reee?"
-    EmitSound("ambient/atmosphere/thunder1.wav", data:GetOrigin(), 0, CHAN_STATIC, 1, 130)
 end)
