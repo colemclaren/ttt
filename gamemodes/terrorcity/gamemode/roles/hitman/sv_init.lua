6185a3344b8da1f44bf5753e915c5a4c69bc44d6
Meepen <meepdarknessmeep@gmail.com>
2018-03-26 00:18:31 -0400

    hitman can no longer get a jester target, can kill jesters and killers without penalties

--- gamemodes/terrorcity/gamemode/roles/hitman/sv_init.lua
+++ gamemodes/terrorcity/gamemode/roles/hitman/sv_init.lua
@@ -1,10 +1,15 @@
 util.AddNetworkString "terrorcity.hitman.acquiretarget"
 local ROLE = ROLE
 
+local ok_roles = {
+    [ROLE_JESTER] = true,
+    [ROLE_KILLER] = true,
+}
+
 local function SelectTarget()
 
     for _, ply in RandomPairs(player.GetAll()) do
-        if (ply:IsDeadTerror() or ply:GetTraitor() or ply:IsSpec()) then
+        if (ply:GetRole() == ROLE_JESTER or ply:IsDeadTerror() or ply:GetTraitor() or ply:IsSpec()) then
             continue
         end
 
@@ -37,9 +42,9 @@ end
 InstallRoleHook("PlayerDeath", 3)
 
 function ROLE:PlayerDeath(victim, inflictor, attacker)
-    if (victim ~= attacker and victim ~= ROLE.Target) then
+    if (victim ~= attacker and victim ~= ROLE.Target and not ok_roles[victim:GetRole()]) then
         attacker:TakeDamage(75, attacker, attacker)
-    else
+    elseif (not ok_roles[victim:GetRole()]) then
         attacker:AddCredits(2)
     end
 end
