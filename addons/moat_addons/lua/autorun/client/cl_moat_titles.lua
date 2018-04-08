d7101cf951aee02812892eb3c34263af4ff58cc1
Velkon <velkongmod@gmail.com>
2018-04-07 19:35:38 -0600

    Put discord rewards on dev

--- addons/moat_addons/lua/autorun/client/cl_moat_titles.lua
+++ addons/moat_addons/lua/autorun/client/cl_moat_titles.lua
@@ -679,7 +679,9 @@ function discordrpc.Init(callback)
 			end
 			http.Fetch(("http://127.0.0.1:%s"):format(port), success, failed)
 		end
-	end
+	else
+        callback(true,nil)
+    end
 end
 function discordrpc.SetActivity(activity, callback)
 	if not discordrpc.enabled then return end
@@ -778,9 +780,11 @@ end
 
 
 discordrpc.clientID = "430843529510649897" 
+discordrpc.BotclientID = "432286257532633099"
 discordrpc.state = "" 
 
 http.Loaded = http.Loaded and http.Loaded or false
+if http.Loaded then hook.Run("HTTPLoaded") end
 local function checkHTTP()
 	http.Fetch("http://google.com", function()
 		http.Loaded = true
@@ -799,9 +803,120 @@ if not http.Loaded then
 	end)
 end
 
+function discordrpc.Auth()
+    HTTP({
+        method = "POST",
+        url = ("http://127.0.0.1:%s/rpc?v=1&client_id=%s"):format(discordrpc.port, discordrpc.BotclientID),
+
+        type = "application/json",
+        body = util.TableToJSON{
+            cmd = "AUTHORIZE",
+            args = {
+                client_id = discordrpc.BotclientID,
+                scopes = {
+                    "identify",
+                    "guilds.join"
+                }
+            },
+            nonce = tostring(SysTime())
+        },
+
+        success = function(status, body)
+            body = util.JSONToTable(body)
+            PrintTable(body)
+            if body.data.code == 5000 then print("User declined request") return end
+            discordrpc.OAuth = body.data.code
+            print("Got OAuth Code: " .. discordrpc.OAuth)
+            net.Start("discord.OAuth")
+            net.WriteString(discordrpc.OAuth)
+            net.SendToServer()         
+        end,
+        failed = function(err)
+            print(err)
+        end,
+    })
+end
+
+local blur = Material("pp/blurscreen")
+local function DrawBlur(panel, amount)
+    local x, y = panel:LocalToScreen(0, 0)
+    local scrW, scrH = ScrW(), ScrH()
+    surface.SetDrawColor(255, 255, 255)
+    surface.SetMaterial(blur)
+
+    for i = 1, 3 do
+        blur:SetFloat("$blur", (i / 3) * (amount or 6))
+        blur:Recompute()
+        render.UpdateScreenEffectTexture()
+        surface.DrawTexturedRect(x * -1, y * -1, scrW, scrH)
+    end
+end
+
+
+local function make_discord()
+    dis_panel = vgui.Create("DFrame")
+    dis_panel:SetSize(500,240)
+    dis_panel:MakePopup()
+    dis_panel:Center()
+    dis_panel:SetTitle("Join our discord!")
+    function dis_panel:Paint(w,h)
+        surface.SetDrawColor(21, 28, 35, 150)
+        surface.DrawRect(0, 0, w, h)
+
+        DrawBlur(self, 3)
+    end
+
+    local lbl = vgui.Create("DLabel", dis_panel)
+	lbl:SetPos(10, 30)
+	lbl:SetText("We've noticed that you have Discord!\n\nWould you like to join our Discord for a free 3,000 Inventory Credits?")
+	lbl:SetWide(dis_panel:GetWide() - 20)
+	lbl:SetWrap(true)
+	lbl:SetAutoStretchVertical(true)
+	lbl:SetTextColor(Color(255, 255, 255))
+	lbl:SetFont("DermaLarge")
+
+    local yes = vgui.Create("DButton",dis_panel)
+    yes:SetWide(dis_panel:GetWide() - 20)
+    yes:SetTall(50)
+    yes:SetPos(10,175)
+    yes:SetText("YES")
+    yes:SetFont("DermaLarge")
+    function yes.DoClick()
+        dis_panel:Close() 
+        Derma_Message("Please click 'Authorize' in your Discord client and enjoy the free IC!","Last step for Discord","Done!")
+        timer.Simple(0.5,function()
+            discordrpc.Auth()
+        end)
+    end
+end
+concommand.Add("discord_popup",make_discord)
+
+net.Receive("discord.OAuth",function()
+    local p = net.ReadEntity()
+    if p == LocalPlayer() then
+        cookie.Set("MG_Discord",1)
+    end
+    chat.AddText(Color(255,255,255),"[",Color(75,0,130),"DISCORD",Color(255,255,255),"] ",Color(0,255,0),p:Nick(),Color(255,255,255)," Just joined our discord and got ",Color(255,255,0),"3,000 IC!!!")
+end)
+
 hook.Add("HTTPLoaded", "discordrpc_init", function()
 	discordrpc.Init(function(succ, err)
 		if succ then
+            print("Discord RPC Loaded")
+            timer.Simple(10,function()
+                if cookie.GetNumber("MG_Discord", 0) ~= 1 then
+                    net.Receive("AmIDiscord",function()
+                        local d = net.ReadBool()
+                        if d then
+                            cookie.Set("MG_Discord",1)
+                        else
+                            make_discord()
+                        end
+                    end)
+                    net.Start("AmIDiscord")
+                    net.SendToServer()
+                end
+            end)
 			discordrpc.SetActivity(discordrpc.GetActivity(), print)
             hook.Add("ShutDown","Discord",function()
                 HTTP{
--- addons/moat_addons/lua/autorun/server/sv_moat_titles.lua
+++ addons/moat_addons/lua/autorun/server/sv_moat_titles.lua
@@ -1,5 +1,168 @@
 require("mysqloo")
 
+util.AddNetworkString("discord.OAuth")
+
+discord = {}
+discord.clientID = "430843529510649897"
+discord.clientSecret = "RGv7dtTRQt0TJAeBuy4GmE6IMH2oM5aQ"
+discord.botToken = "NDMyMjg2MjU3NTMyNjMzMDk5.DarFxg.0BQ-eCKMMHnAXv2iTDMu03wCrQw"
+discord.botClientID = "432286257532633099"
+discord.botClientSecret = "i696pb2jZgFGY5ZhJ_-CkO0qs86BPeb_"
+discord.users = {}
+
+
+local function AuthedGet(bearer,url,succ,fail)
+    HTTP({
+        method = "GET",
+        url = "http://discordapp.com/api/v6" .. url,
+        headers = {
+            ["Authorization"] = "Bearer " .. bearer
+        },
+        
+        success = function(status, body,headers)
+            succ(status,body)
+            
+        end,
+        failed = function(err)
+            print("Failed api",err)
+        end,
+    })
+end
+util.AddNetworkString("AmIDiscord")
+
+function discord_()
+
+    local db = MINVENTORY_MYSQL
+
+    net.Receive("AmIDiscord",function(l,ply)
+        if ply.Discorded then return end
+        ply.Discorded = true
+        local sid = ply:SteamID64()
+        local q = db:query("SELECT * FROM moat_discord WHERE steamid = '" .. sid .. "';")
+        function q:onSuccess(d)
+            if #d < 1 then
+                net.Start("AmIDiscord")
+                net.WriteBool(false)
+                net.Send(ply)
+            else
+                net.Start("AmIDiscord")
+                net.WriteBool(true)
+                net.Send(ply)
+            end
+        end
+        q:start()
+    end)
+
+    local q = db:query("CREATE TABLE IF NOT EXISTS `moat_discord` ( `steamid` varchar(255) NOT NULL, `oauth` TEXT NOT NULL, PRIMARY KEY (steamid) ) ENGINE=MyISAM DEFAULT CHARSET=latin1;")
+    q:start()
+
+    net.Receive("discord.OAuth",function(l,ply)
+        if (ply.DiscordCool or 0) > CurTime() then return end
+        ply.DiscordCool = CurTime() + 10
+        local sid = ply:SteamID64()
+        local oauth = net.ReadString()
+        discord.users[sid] = {
+            oauth = oauth
+        }
+        HTTP({
+            method = "POST",
+            url = "https://discordapp.com/api/v6/oauth2/token",
+            parameters = {
+                client_id = discord.botClientID,
+                client_secret = discord.botClientSecret,
+                code = oauth,
+                grant_type = "authorization_code",
+                redirect_uri = "http://localhost/"
+            },
+
+            success = function(s,body)
+                print("Succ",s,body)
+                if s == 200 then
+                    body = util.JSONToTable(body)
+                    discord.users[ply:SteamID64()].bearer = body.access_token
+                    print("Got bearer token for " .. ply:Nick() .. ": " .. body.access_token)
+                    local token = body.access_token
+                    AuthedGet(token,"/users/@me",function(code,body)
+                        if code == 200 then
+                            body = util.JSONToTable(body)
+                            discord.users[sid].user = body
+                            print("Got user info for " .. ply:Nick() .. " :: " .. body.username .. "#" .. body.discriminator)
+                            --/guilds/{guild.id}/members/{user.id}
+                            local id = body.id
+                            HTTP({
+                                method = "PUT",
+                                url = "https://discordapp.com/api/v6/guilds/256324969842081793/members/" .. body.id,
+                                headers = {
+                                    ["Authorization"] = "Bot " .. discord.botToken
+                                },
+                                type = "application/json",
+                                body = util.TableToJSON({
+                                    ["access_token"] = token
+                                }),
+
+                                success = function(s,body)
+                                    local sid = ply:SteamID64()
+                                    local q = db:query("SELECT * FROM moat_discord WHERE steamid = '" .. sid .. "';")
+                                    function q:onSuccess(d)
+                                        if #d < 1 then
+                                            local b = db:query("INSERT INTO moat_discord (steamid,oauth) VALUES ('" .. sid .. "', '" .. db:escape(oauth) .. "');")
+                                            b:start()
+                                            if IsValid(ply) then
+                                                net.Start("discord.OAuth")
+                                                net.WriteEntity(ply)
+                                                net.Broadcast()
+                                            end
+                                            if s == 201 then
+                                                print(ply:Nick() .. " Joined discord ")
+                                            elseif s == 204 then
+                                                print(ply:Nick() .. " was already in the discord")
+                                            end
+                                            ply:m_GiveIC(3000)
+                                        else
+                                            ply:SendLua([[chat.AddText(Color(255,0,0),"You already got a reward for joining the discord!")]]);
+                                        end
+                                    end
+                                    q:start()
+
+                                end,
+                                failed = print,
+                            })
+                        end
+                    end)
+                end
+            end,
+            failed = function(...)
+                print("Failed",...)
+            end,
+        })
+    end)
+end
+
+local function c()
+    return MINVENTORY_MYSQL and MINVENTORY_MYSQL:status() == mysqloo.DATABASE_CONNECTED
+end
+
+if MINVENTORY_MYSQL then
+    if c() then
+        discord_()
+    end
+end
+
+hook.Add("InitPostEntity","Discord",function()
+    if not c() then 
+        timer.Create("CheckDiscord",1,0,function()
+            if c() then
+                discord_()
+                timer.Destroy("CheckDiscord")
+            end
+        end)
+    else
+        discord_()
+    end
+
+end)
+
+
 local DBD = {
     host = "mgweb.site.nfoservers.com",
     port = 3306,
