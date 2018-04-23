92263d78395e48446e59a0ad3066d9aa8de77ece
moat7 <cole92107@yahoo.com>
2018-04-22 17:05:27 -0700

    :ac updates

--- addons/moat_addons/lua/autorun/client/mac.lua
+++ addons/moat_addons/lua/autorun/client/mac.lua
@@ -1,4 +1,4 @@
-/*local setmetatable = _G.setmetatable
+local setmetatable = _G.setmetatable
 
 local function readonly(tab)
     return setmetatable({}, {
@@ -240,5 +240,4 @@ scan(function()
             end
         end
     end
-end)*/
-print "no"
+end)
