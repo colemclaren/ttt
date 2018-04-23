5c31e1dd0bb93c67ecf690b140064bf07a68c7bf
moat7 <cole92107@yahoo.com>
2018-04-22 19:19:39 -0700

    :remove unneeded files

--- addons/moat_addons/lua/autorun/slvbase/client/slvbase_cl_meta.lua
+++
@@ -1,9 +0,0 @@
-local meta = FindMetaTable("NPC")
-function meta:GetWeapon(class)
-	for k, v in pairs(ents.FindByClass(class)) do
-		if v.Owner == self then
-			return v
-		end
-	end
-	return NULL
-end
--- addons/moat_addons/lua/autorun/slvbase/shared/slvbase_sh_hooks.lua
+++
@@ -1,3 +0,0 @@
-hook.Add("ShouldCollide", "HLR_ShouldCollide", function(entA, entB)
-	if(entA:CanCollide(entB) == false) then return false end
-end)
