66ff0cf861b30bc5ce8cc36f1adbd02063546b18
Velkon <velkongmod@gmail.com>
2018-05-03 18:52:13 -0600

    :Stop spec bans

--- addons/moat_addons/lua/autorun/server/moat_tradebans.lua
+++ addons/moat_addons/lua/autorun/server/moat_tradebans.lua
@@ -52,6 +52,8 @@ end)
 hook.Add("StartCommand", "Joystick", function(p, c)
 	if (p.MCoolDown or 0) > CurTime() then return end
 	local mwheel = c:GetMouseWheel()
+    if MOAT_MINIGAME_OCCURING then return end
+    if p:IsSpec() then return end
     if (not p:InVehicle() and not c:IsForced() and not p:IsBot() and not (mwheel == 0 or mwheel == 127)) and (p:Alive()) and (mwheel <= -2) then
         print(mwheel)
 		p.MDetect = true
