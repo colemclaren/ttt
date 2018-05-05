9a158bd5841e17afe73181a67b1082fa716da8a3
Velkon <velkongmod@gmail.com>
2018-05-04 23:12:11 -0600

    :Disable versus on servers that dont have moat in name

--- addons/moat_addons/lua/autorun/server/moat_gambleserver.lua
+++ addons/moat_addons/lua/autorun/server/moat_gambleserver.lua
@@ -930,6 +930,7 @@ function jackpot_()
     end)
     timer.Create("Versus.Watchdog",10,0,function()
         if GG_DISABLE:GetBool() then return false end
+        if not GetHostName():lower():match("moat") then return end
         versus_getgames(function(d)
             local games = {}
             for k,v in pairs(d) do
