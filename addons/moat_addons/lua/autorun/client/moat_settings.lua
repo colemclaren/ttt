d227987879248d8144e8cb19bb306c8f67b76db5
Velkon <velkongmod@gmail.com>
2018-05-22 17:34:11 -0600

    Ignore fov for apache

--- addons/moat_addons/lua/autorun/client/moat_settings.lua
+++ addons/moat_addons/lua/autorun/client/moat_settings.lua
@@ -86,6 +86,7 @@ local moat_rarity_colors = {
 hook.Add("CalcView","Change FOV",function(ply, pos, angles, fov)
     if MOAT_IGNORE_FOV then return end
     if cur_random_round then return end
+    if MOAT_ACTIVE_BOSS then return end
     if (not LocalPlayer():Alive()) or LocalPlayer():IsSpec() then return end
     if current_scene then return end
     if cur_random_round == "High FOV" then return end
