e305209cd698817368ab9917f01da73a9d178fc4
moatato <moat@moat.gg>
2018-05-25 21:35:18 -0700

    fix beacon lightning script error again lol

--- gamemodes/terrorcity/gamemode/roles/beacon/cl_init.lua
+++ gamemodes/terrorcity/gamemode/roles/beacon/cl_init.lua
@@ -7,7 +7,7 @@ net.Receive("tc_beacon_pos", function(len, ply)
 
     sound.PlayURL("https://i.moat.gg/DRx6h.mp3", "3d", function(snd)
         if (IsValid(snd)) then
-            snd:SetPos(data)
+            snd:SetPos(vec)
             snd:Set3DFadeDistance(8000, 1000000000)
             snd:Play()
         end
