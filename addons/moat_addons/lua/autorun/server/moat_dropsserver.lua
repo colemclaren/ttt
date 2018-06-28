d07f9b5a23b45b2ec8f806377b425e0a5a0e9679
Velkon <velkongmod@gmail.com>
2018-06-28 07:25:05 +0300

    Revert "Add command to find unused itemid"
    This reverts commit 4d8ed926a86cb9e8ab0dd7179f6eacdebc7708e7.

--- addons/moat_addons/lua/autorun/server/moat_dropsserver.lua
+++ addons/moat_addons/lua/autorun/server/moat_dropsserver.lua
@@ -143,7 +143,7 @@ function m_InitializeTalents()
 end
 m_InitializeTalents()
 
-concommand.Add("moat_finditemid",function(ply) --commit comment
+concommand.Add("moat_finditemid",function(ply)
     if (IsValid(ply)) then return end
     for i = 1,10000 do
         if (not MOAT_DROPTABLE[i]) then
