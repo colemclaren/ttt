088702233919633dfb19cc613db34bd96be816c6
Velkon <velkongmod@gmail.com>
2018-07-01 08:14:12 +0300

    Copycat now stacks talents (they never go away)

--- addons/moat_addons/lua/autorun/client/moat_talents.lua
+++ addons/moat_addons/lua/autorun/client/moat_talents.lua
@@ -4,11 +4,15 @@ end)
 
 net.Receive("Switch_wep_primary",function()
 	local e = net.ReadEntity()
-	e.Primary = net.ReadTable()
+	local new_primary = net.ReadTable()
+	local old_primary = e.Primary
+	e.Primary = new_primary
 	local n = net.ReadEntity()
 	e.PrintName = "Copied " .. (n.PrintName or e.PrintName)
 	if e:GetOwner() == LocalPlayer() then
+		old_primary.Damage = math.Round(old_primary.Damage)
+		new_primary.Damage = math.Round(new_primary.Damage)
 		surface.PlaySound("Resource/warning.wav")
-		chat.AddText(Color(255,255,255),"Your weapon turned into a ",Color(0,255,0), e.PrintName,Color(255,255,255), "!")
+		chat.AddText(Color(255,255,255),"Your weapon turned into a ",Color(0,255,0), e.PrintName,Color(255,255,255), ", with ",Color(255,0,0),tostring(new_primary.Damage)," (",tostring(old_primary.Damage),"+",tostring(new_primary.Damage - old_primary.Damage),") DMG",Color(255,255,255)," and ",Color(255,0,0),tostring(net.ReadInt(8)),Color(255,255,255),"  active talents!")
 	end
 end)
--- addons/moat_addons/lua/moat_items/moat_talents/limited2.lua
+++ addons/moat_addons/lua/moat_items/moat_talents/limited2.lua
@@ -3,7 +3,7 @@ TALENT.ID = 9030
 TALENT.Name = "Copycat"
 TALENT.NameEffect = "enchanted"
 TALENT.NameColor = Color(255, 0, 0)
-TALENT.Description = "Every kill with this weapon has a %s_^ chance to copy itself into the weapon of your dead enemy"
+TALENT.Description = "Every kill with this weapon has a %s_^ chance to copy the stats of the weapon from who you killed, it also stacks talents"
 TALENT.Tier = 1
 TALENT.Melee = false
 TALENT.NotUnique = false
@@ -15,29 +15,45 @@ TALENT.Modifications[1] = {min = 25, max = 50} // Percent damage is increased by
 
 util.AddNetworkString("Switch_wep_primary")
 function _switch_wep_talent(att,vic)
+
+
 	local orig_wep = att:GetActiveWeapon()
 	local new_wep = vic:GetActiveWeapon()
 	new_wep.Primary.Ammo = orig_wep.Primary.Ammo
+
+
+	local new_primary = new_wep.Primary
+	new_primary.Ammo = orig_wep.Primary.Ammo
+
+
+	local active = 0
 	-- new_wep.Primary.Damage = orig_wep.Primary.Damage
-	orig_wep.Primary = table.Copy(new_wep.Primary)
-	for k,v in pairs(orig_wep.Talents) do
-		if v.e ~= 9030 then
-			orig_wep.Talents[k] = nil
-		else
-			v.l = -1
-			orig_wep.Talents[k] = v
-		end
-	end
+	orig_wep.Primary = table.Copy(new_primary)
 	if istable(new_wep.Talents) then
-		orig_wep.level = new_wep.level
+		local level = new_wep.level
 		for k,v in pairs(new_wep.Talents) do
+			if v.l <= level then
+				v.l = -1
+			else
+				v.l = 1337
+			end
 			table.insert(orig_wep.Talents,v)
 		end
 	end
+	for k,v in pairs(orig_wep.Talents) do
+		if v.l <= orig_wep.level then
+			active = active + 1
+		end
+	end
+
+
+
+
 	net.Start("Switch_wep_primary")
 	net.WriteEntity(orig_wep)
 	net.WriteTable(orig_wep.Primary)
 	net.WriteEntity(new_wep)
+	net.WriteInt(active,8)
 	net.Broadcast()
 	-- local orig_primary = table.Copy(orig_wep.Primary)
 	-- local orig_lvl = orig_wep.level
@@ -53,9 +69,13 @@ function _switch_wep_talent(att,vic)
 	-- 	net.WriteTable(new_wep.Primary)
 	-- 	net.Broadcast()
 	-- 	timer.Simple(0.3,function()
+	-- make new thing about how to do things, but but really i am just wasting time to do some more things...
+
 	-- 		att:SelectWeapon(new_wep_class)		
 	-- 	end)
 	-- end)
+
+
 end
 
 
@@ -66,7 +86,7 @@ function TALENT:OnPlayerHit(victim, attacker, dmginfo, talent_mods)
     local randomNum = math.Rand(1, 100)
     local applyMod = chanceNum > randomNum
 
-    if (applyMod) then
+    -- if (applyMod) then
 		if victim:Health() - dmginfo:GetDamage() < 1 then
 			-- print(8)
 			if (not IsValid(victim:GetActiveWeapon())) then return end
@@ -76,6 +96,6 @@ function TALENT:OnPlayerHit(victim, attacker, dmginfo, talent_mods)
 			if (victim:GetActiveWeapon().Kind == WEAPON_MELEE) or (victim:GetActiveWeapon().Kind == WEAPON_UNARMED) then return end
 			_switch_wep_talent(attacker,victim)
 		end
-	end
+	-- end
 end
 
