af06c5d30ffd1d95bb8aedaff79dc7d8295b1c53
Velkon <velkongmod@gmail.com>
2018-09-04 18:43:34 -0600

    Take ttt_mario_b1 out of map pool

--- addons/moat_addons/lua/system/constants/maps/sh_blacklist.lua
+++ addons/moat_addons/lua/system/constants/maps/sh_blacklist.lua
@@ -10,5 +10,6 @@ MAP_BLACKLIST = {
 	["ttt_skybreak_v2"] = true, -- re-add the map when krazy is finished
 	["ttt_mc_bonfire"] = true, -- player collisions on spawn causes death
 	["ttt_castillo_de_fenrir"] = true, -- removed from workshop?
-	["ttt_mc_kitchen"] = true -- updated on workshop
+	["ttt_mc_kitchen"] = true, -- updated on workshop
+	["ttt_mario_b1"] = true -- https://i.imgur.com/hNGjcKd.png
 }
