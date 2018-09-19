892bd7942e09a7825b7de06dae9f60aeb465a188
Velkon <velkongmod@gmail.com>
2018-09-18 22:08:56 -0600

    *Reword silenced talent description *Cache silenced stat in preparing round

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/silenced.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/silenced.lua
@@ -2,7 +2,7 @@
 TALENT.ID = 9040
 TALENT.Name = "Silenced"
 TALENT.NameColor = Color(0, 255,0)
-TALENT.Description = "Your weapon has a %s_^ chance to become silenced each round"
+TALENT.Description = "Each round, your weapon has a %s_^ chance to become silenced"
 TALENT.Tier = 1
 TALENT.LevelRequired = {min = 5, max = 10}
 TALENT.Modifications = {}
@@ -12,6 +12,11 @@ TALENT.NotUnique = true
 
 util.AddNetworkString("Talents.Silenced")
 --s
+silence_prep_cache = {}
+hook.Add("TTTBeginRound","ClearSilenced",function()
+	silence_prep_cache = {}
+end)
+
 function silence_weapon_talent(weapon)
 	net.Start("Talents.Silenced")
 	net.WriteEntity(weapon)
@@ -22,6 +27,15 @@ function TALENT:ModifyWeapon( weapon, talent_mods )
     local chanceNum = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
     local randomNum = math.Rand(1, 100)
     local applyMod = chanceNum > randomNum
+	if not silence_prep_cache[weapon.Weapon:GetOwner()] then
+		silence_prep_cache[weapon.Weapon:GetOwner()] = {}
+	end
+
+	if silence_prep_cache[weapon.Weapon:GetOwner()][weapon.Weapon:GetClass()] then
+		applyMod = silence_prep_cache[weapon.Weapon:GetOwner()][weapon.Weapon:GetClass()]
+	else
+		silence_prep_cache[weapon.Weapon:GetOwner()][weapon.Weapon:GetClass()] = applyMod
+	end
 
     if (applyMod) then
     	silence_weapon_talent(weapon.Weapon)
