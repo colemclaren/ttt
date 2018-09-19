cb5916abe2c5ce6cf12d6e3b0259bb48513d29f3
Velkon <velkongmod@gmail.com>
2018-09-18 22:10:07 -0600

    *Raise silenced price to 49k

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/tiers/limited5.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/tiers/limited5.lua
@@ -30,6 +30,6 @@ ITEM.MinTalents = 2
 ITEM.MaxTalents = 3
 ITEM.Talents = {"Silenced", "random", "random"}
 
-ITEM.Price = 44999
+ITEM.Price = 49999
 ITEM.LimitedShop = 1537758231
 ITEM.ShopDesc = "Limited time weapon that comes with the Silenced talent!\nThe Silenced talent is also available normally, so buying this is not necessary.\nEspecially with such a high price, this item is meant for collectors!\nBuying will give you a random weapon!"
