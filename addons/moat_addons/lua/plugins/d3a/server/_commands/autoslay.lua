277afaf68b85d7bba0b2d60295b4cab74fc63f7a
Velkon <velkongmod@gmail.com>
2018-09-25 16:38:15 -0600

    Fix aslay

--- addons/moat_addons/lua/plugins/d3a/server/_commands/autoslay.lua
+++ addons/moat_addons/lua/plugins/d3a/server/_commands/autoslay.lua
@@ -6,54 +6,7 @@ COMMAND.CheckRankWeight = true
 
 COMMAND.Args = {{"player", "Name/SteamID"}, {"number", "Rounds/0 to Cancel"}, {"string", "Reason"}}
 
-COMMAND.CheckArgs = function(pl, cmd, args)
-	local margs = cmd.Args
-	local err
-	local supp = false
-	
-	if ((pl and not pl.rcon) and pl:IsPlayer() and !pl:HasAccess(cmd.Flag)) then
-		err = "'" .. cmd.Flag .. "' access required!"
-	end
-	
-	if (!err) then
-		for k, v in pairs(args) do
-			if (!args[k]) then
-				err = "_"
-				break
-			end
-			
-			local targ = D3A.FindPlayer(v)
-			if (targ) then
-				if (cmd.CheckRankWeight) then
-					if (!D3A.Ranks.CheckWeight(pl, targ)) then
-						D3A.Chat.SendToPlayer2(pl, moat_red, "Player's rank is equal or greater weight than yours!")
-						D3A.Chat.SendToPlayer2(targ, moat_red, pl:Name() .. " (" .. pl:SteamID() .. ") attempted to use " .. cmd.Name .. " on you.")
-						continue
-					end
-				end
-				supp = supp or {}
-				table.insert(supp, targ)
-			end
-		end
-	end
-	
-	if (!err and (!supp or #supp == 0)) then
-		err = "_"
-	end
-	
-	if (err) then
-		if (err == "_") then
-			err = "Usage: " .. cmd.Name .. " "
-			for k, v in pairs(margs) do
-				err = err .. "[" .. v[1] .. ":" .. v[2] .. "] "
-			end
-		end
-		D3A.Chat.SendToPlayer2(pl, moat_red, err)
-		return false
-	end
-	
-	return supp
-end
+-- checkargs is automatic now
 
 COMMAND.Run = function(pl, args, supplement)
 	local plname = (((pl and pl.rcon) or IsValid(pl)) and pl:Name()) or "Console"
