f16cb8a5a53d2ca03f64bfff048f49d57ba4e28a
moat <moat@moat.gg>
2018-09-28 18:55:09 -0700

    ^ Update player and servers modules
    ^ Add more stuff to markdown library for discord messages
    ^ Make map events look cooler and move them to discord general

---
+++ addons/moat_addons/lua/system/constants/github/sv_bad_webhooks.lua
@@ -0,0 +1,6 @@
+-- until we fix discord webhook
+-- u can use this if u multi commit
+
+-- 55971171473fad697eac6b1fb860fa5ca9302d95
+-- c4d1a90a7f53a403a3f7e81f618fbfd459124f68
+-- 6a1ff677c4588a5d2fd8303d4b3547491b0e7921
