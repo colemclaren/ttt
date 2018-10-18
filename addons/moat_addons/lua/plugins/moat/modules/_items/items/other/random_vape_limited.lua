5879cc6768e8fd552160dda691e19942d636be02
Velkon <velkongmod@gmail.com>
2018-10-17 22:40:49 -0600

    Lower random vape price to 54.99k

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/other/random_vape_limited.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/other/random_vape_limited.lua
@@ -6,6 +6,6 @@ ITEM.Image = "https://cdn.moat.gg/f/Rx0pUsUKgsDXYUzidrKNqe0L2yQS.png"
 ITEM.Collection = "Shouldn't be in inventory"
 ITEM.WeaponClass = "weapon_ttt_unarmed"
 
-ITEM.Price = 69999
+ITEM.Price = 54999
 ITEM.LimitedShop = 1540438149
 ITEM.ShopDesc = "For a limited time, you can purchase a random vape!\nYou also have a CHANCE TO GET A MEGA VAPE!\n\n(Normal Vapes can be used unlimited times whenever you want,\nMega vapes are the same but make BIG smoke every 12 hours)"
