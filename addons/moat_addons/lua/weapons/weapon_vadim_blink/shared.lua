11ac008d86346704722db9a80234922dc406a320
Meepen <meepdarknessmeep@gmail.com>
2018-10-29 00:31:49 -0400

    blink fixes and updates

--- addons/moat_addons/lua/weapons/weapon_vadim_blink/shared.lua
+++ addons/moat_addons/lua/weapons/weapon_vadim_blink/shared.lua
@@ -29,7 +29,6 @@ SWEP.Primary.DefaultClip = -1
 SWEP.Primary.Automatic = false
 SWEP.Primary.Delay = 1
 SWEP.Primary.Ammo = "none"
-SWEP.Charge = 0
 SWEP.Timer = -1
 -- settings
 local cvf = FCVAR_ARCHIVE + FCVAR_REPLICATED + FCVAR_SERVER_CAN_EXECUTE
@@ -45,93 +44,110 @@ SWEP.ViewModel = "models/weapons/c_slam.mdl"
 SWEP.WorldModel = "models/weapons/w_slam.mdl"
 SWEP.AutoSpawnable = false
 SWEP.NoSights = true
-SWEP.Active = -1
 SWEP.Ghost = nil
-SWEP.BlinkInfo = false
-SWEP.MaxCharge = 100
-SWEP.Charge = 100
 -- content loading
 local aim = {Sound("vadim_blink/aim1.mp3"), Sound("vadim_blink/aim2.mp3")}
 local tpl = {Sound("vadim_blink/teleport1.mp3"), Sound("vadim_blink/teleport2.mp3")}
-local inv = Material("models/effects/vol_light001")
-local vis = Material("")
 
-if SERVER then
-    function RagdollDissolveEffect(ply)
-        if not IsValid(BLINK_DISSOLVER) then
-            BLINK_DISSOLVER = ents.Create("env_entity_dissolver")
-            BLINK_DISSOLVER:SetKeyValue("dissolvetype", 3)
-            BLINK_DISSOLVER:Spawn()
-        end
-
-        if SERVER and IsValid(ply) then
-            local rgd = ents.Create("prop_ragdoll")
-            if not IsValid(rgd) then return nil end
-            rgd:SetPos(ply:GetPos())
-            rgd:SetModel(ply:GetModel())
-            rgd:SetAngles(ply:GetAngles())
-            rgd:Spawn()
-            rgd:Activate()
-            rgd:SetGravity(0)
-            rgd:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
-            rgd:SetMaxHealth(100)
-            rgd:SetHealth(100)
-            rgd.Owner = ply
-            rgd:SetOwner(ply)
-            rgd:SetColor(Color(255, 255, 255, 100))
-
-            if IsValid(rgd) then
-                for i = 0, rgd:GetPhysicsObjectCount() - 1 do
-                    local phys = rgd:GetPhysicsObjectNum(i)
-                    phys:Wake()
-
-                    if IsValid(phys) then
-                        local pos, ang = ply:GetBonePosition(rgd:TranslatePhysBoneToBone(i))
-                        phys:EnableGravity(false)
-
-                        if pos and ang then
-                            phys:SetPos(pos)
-                            phys:SetAngles(ang)
-                        end
-                    end
-
-                    phys:EnableMotion(false)
-
-                    timer.Simple(0.1, function()
-                        phys:Sleep()
-                    end)
-                end
-            end
-
-            rgd:SetName("DissolveID" .. rgd:EntIndex())
-            BLINK_DISSOLVER:Fire("Dissolve", "DissolveID" .. rgd:EntIndex(), 0.01)
-        end
+local function RagdollDissolveEffect(ply)
+    if not IsValid(BLINK_DISSOLVER) then
+        BLINK_DISSOLVER = ents.Create("env_entity_dissolver")
+        BLINK_DISSOLVER:SetKeyValue("dissolvetype", 3)
+        BLINK_DISSOLVER:Spawn()
     end
+
+    if SERVER and IsValid(ply) then
+        local rgd = ents.Create("prop_ragdoll")
+        if not IsValid(rgd) then return nil end
+        rgd:SetPos(ply:GetPos())
+        rgd:SetModel(ply:GetModel())
+        rgd:SetAngles(ply:GetAngles())
+        rgd:Spawn()
+        rgd:Activate()
+        rgd:SetGravity(0)
+        rgd:SetCollisionGroup(COLLISION_GROUP_DEBRIS_TRIGGER)
+        rgd:SetMaxHealth(100)
+        rgd:SetHealth(100)
+        rgd.Owner = ply
+        rgd:SetOwner(ply)
+        rgd:SetColor(Color(255, 255, 255, 100))
+
+        if IsValid(rgd) then
+            for i = 0, rgd:GetPhysicsObjectCount() - 1 do
+                local phys = rgd:GetPhysicsObjectNum(i)
+                phys:Wake()
+
+                if IsValid(phys) then
+                    local pos, ang = ply:GetBonePosition(rgd:TranslatePhysBoneToBone(i))
+                    phys:EnableGravity(false)
+
+                    if pos and ang then
+                        phys:SetPos(pos)
+                        phys:SetAngles(ang)
+                    end
+                end
+
+                phys:EnableMotion(false)
+
+                timer.Simple(0.1, function()
+                    phys:Sleep()
+                end)
+            end
+        end
+
+        rgd:SetName("DissolveID" .. rgd:EntIndex())
+        BLINK_DISSOLVER:Fire("Dissolve", "DissolveID" .. rgd:EntIndex(), 0.01)
+    end
+end
+
+function SWEP:SetupDataTables()
+    self:NetworkVar("Bool", 0, "Attacking")
+    self:NetworkVar("Bool", 1, "Blinking")
+    self:NetworkVar("Float", 0, "BlinkTime")
+    self:NetworkVar("Float", 1, "ChargeUpdate")
+    self:NetworkVar("Float", 2, "MaxCharge")
+    self:NetworkVar("Float", 3, "ImmunityTime")
+    self:NetworkVar("Float", 4, "ChargeUpdateTime")
+    self:NetworkVar("Vector", 0, "BlinkPos")
+    self:NetworkVar("Vector", 1, "OriginPos")
+end
+
+function SWEP:GetCharge()
+    return math.min(self:GetChargeUpdate() + 15 * (CurTime() - self:GetChargeUpdateTime()), self:GetMaxCharge())
+end
+
+function SWEP:SetCharge(val)
+    self:SetChargeUpdate(val)
+    self:SetChargeUpdateTime(CurTime())
 end
 
 -- chets
 function SWEP:Uncloak(ply)
-    timer.Simple(0.75, function()
-        if IsValid(ply) then
-            ply.BlinkImmunity = nil
-        end
-    end)
-
     if (cur_random_round and cur_random_round ~= "Invisible Traitors") then
         ply:SetMaterial("")
         ply:SetRenderMode(RENDERMODE_NORMAL)
-        
+
         if CLIENT then return end
         ply:Fire("alpha", 255, 0.16)
     end
 end
 
 function SWEP:PrimaryAttack()
+    if (self:GetBlinking() or self:GetCharge() < takecharge:GetInt()) then
+        return false
+    end
+    self:SetAttacking(true)
+    self:SetBlinkTime(CurTime())
+    if CLIENT then
+        self.Ghost:SetSequence(self:GetOwner():GetSequence())
+    end
+
+    self:EmitSound(aim[math.random(1, 2)])
     return false
 end
 
 function SWEP:SecondaryAttack()
-    self.Active = -2
+    self:SetAttacking(false)
     self:Uncloak(self.Owner)
 end
 
@@ -149,42 +165,40 @@ function SWEP:DrawWorldModel()
     end
 end
 
-function SWEP:Initialize()
-    if CLIENT then
-        local blinkmod = {
-            ["$pp_colour_addr"] = 0,
-            ["$pp_colour_addg"] = 0,
-            ["$pp_colour_addb"] = 0,
-            ["$pp_colour_brightness"] = -0.1,
-            ["$pp_colour_contrast"] = 1.25,
-            ["$pp_colour_colour"] = 0,
-            ["$pp_colour_mulr"] = 0,
-            ["$pp_colour_mulg"] = 0,
-            ["$pp_colour_mulb"] = 0
-        }
+local blinkmod = {
+    ["$pp_colour_addr"] = 0,
+    ["$pp_colour_addg"] = 0,
+    ["$pp_colour_addb"] = 0,
+    ["$pp_colour_brightness"] = -0.1,
+    ["$pp_colour_contrast"] = 1.25,
+    ["$pp_colour_colour"] = 0,
+    ["$pp_colour_mulr"] = 0,
+    ["$pp_colour_mulg"] = 0,
+    ["$pp_colour_mulb"] = 0
+}
 
-        local efade = 0.10
-        hook.Remove("RenderScreenspaceEffects", "BlinkPP")
+local efade = 0.10
 
-        hook.Add("RenderScreenspaceEffects", "BlinkPP", function()
-            local wep = LocalPlayer():GetActiveWeapon()
+hook.Add("RenderScreenspaceEffects", "BlinkPP", function()
+    local wep = LocalPlayer():GetActiveWeapon()
 
-            if wep and IsValid(wep) and wep.Active and wep.Active > 0 then
-                local iter = math.min(1, 1 - ((wep.Active + efade) - CurTime()) / efade)
-                blinkmod["$pp_colour_addb"] = iter * 0.05
-                blinkmod["$pp_colour_brightness"] = iter * -0.15
-                blinkmod["$pp_colour_mulb"] = iter
-                blinkmod["$pp_colour_contrast"] = 1 + iter * 0.05
-                blinkmod["$pp_colour_colour"] = math.max(0.2, 1 * (1 - iter))
-                DrawColorModify(blinkmod)
-                DrawBloom(1 - iter * 0.2, iter * 8, iter * 4, iter * 8, 2, iter * 0.5, iter * 0.8, iter * 0.8, iter * 0.9)
-            end
-        end)
+    if wep and IsValid(wep) and wep:GetClass() == "weapon_vadim_blink" and wep:GetAttacking() then
+        local iter = math.min(1, 1 - ((wep:GetBlinkTime() + efade) - CurTime()) / efade)
+        blinkmod["$pp_colour_addb"] = iter * 0.05
+        blinkmod["$pp_colour_brightness"] = iter * -0.15
+        blinkmod["$pp_colour_mulb"] = iter
+        blinkmod["$pp_colour_contrast"] = 1 + iter * 0.05
+        blinkmod["$pp_colour_colour"] = math.max(0.2, 1 * (1 - iter))
+        DrawColorModify(blinkmod)
+        DrawBloom(1 - iter * 0.2, iter * 8, iter * 4, iter * 8, 2, iter * 0.5, iter * 0.8, iter * 0.8, iter * 0.9)
     end
+end)
 
+function SWEP:Initialize()
+    hook.Add("FinishMove", self, self.FinishMove)
     self:SetHoldType(self.HoldType)
-    self.Charge = maxcharge:GetInt()
-    self.MaxCharge = maxcharge:GetInt()
+    self:SetCharge(maxcharge:GetInt())
+    self:SetMaxCharge(maxcharge:GetInt())
 end
 
 function SWEP:CreateGhost()
@@ -201,95 +215,70 @@ end
 
 function SWEP:Deploy()
     self.Owner:GetViewModel():SendViewModelMatchingSequence(12)
-    self.BlinkInfo = false
 
-    if IsValid(self.Last) then
-        self:Uncloak(self.Last)
-    end
-
+    self:SetAttacking(false)
+    self:SetBlinking(false)
     return true
 end
 
-if (CLIENT) then 
+if (CLIENT) then
     function SWEP:Remove()
         hook.Remove("RenderScreenspaceEffects", "BlinkPP")
     end
 end
 
-function SWEP:Holster()
-    self.Active = -1
-    self.BlinkInfo = false
-
-    if IsValid(self.Last) then
-        self:Uncloak(self.Last)
-    end
-
-    return true
-end
-
 function SWEP:OnDrop()
-    self.Active = -1
-    self.BlinkInfo = false
     self.FOV = nil
 
-    if IsValid(self.Last) then
-        self:Uncloak(self.Last)
-    end
-
-    return true
+    self:SetBlinking(false)
+    self:SetAttacking(false)
 end
 
 -- trace mechanics
 function SWEP:Trace()
     local own = self.Owner
-    local phys = own:GetPhysicsObject()
-    local size = Vector(34, 34, 74)
+    local mins, maxs = own:GetHull()
 
-    local tr0 = util.TraceHull({
-        start = own:EyePos() + own:GetAimVector(),
+
+    local t = {
+        start = own:EyePos(),
         endpos = own:EyePos() + own:GetAimVector() * range:GetFloat(),
-        mins = size / -4,
-        maxs = size / 4,
-        mask = MASK_ALL,
-        filter = own
-    })
+        mins = mins,
+        maxs = maxs,
+        mask = MASK_PLAYERSOLID,
+        filter = own,
+    }
 
-    local final = tr0.HitPos - Vector(0, 0, size.z / 2)
-    local ledge = final + Vector(0, 0, size.z)
+    local tr = util.TraceLine(t)
 
-    local tr1 = util.TraceLine({
-        start = ledge,
-        endpos = ledge + own:GetAimVector() * size.y * 2,
-        mask = MASK_ALL,
-        filter = own
-    })
+    t.endpos = t.start
+    t.start = tr.HitPos
+    local start, endpos = t.start, t.endpos
+    tr = util.TraceHull(t)
 
-    if not tr1.Hit then
-        local tr2 = util.TraceLine({
-            start = tr1.HitPos,
-            endpos = tr1.HitPos - Vector(0, 0, size.z),
-            mask = MASK_ALL,
-            filter = own
-        })
+    t.start = own:GetPos()
+    t.endpos = own:GetPos() + own:GetAimVector() * range:GetFloat()
+    local tr2 = util.TraceHull(t)
 
-        if tr2.Hit then
-            final = tr2.HitPos + Vector(0, 0, 8)
-        end
+    local final
+    if (tr.AllSolid) then
+        final = tr2.HitPos
+    elseif (tr.StartSolid) then
+        final = start + (endpos - start) * tr.FractionLeftSolid
+    else
+        final = tr.StartPos
     end
-    -- go look for a ledge ya bugger
 
-    return final, tr0.HitNormal
+    return final, tr.HitNormal
 end
 
-local test = CurTime()
+function SWEP:Holster()
+    return not self:GetAttacking()
+end
 
 -- thonkang
 function SWEP:Think()
-    local own = self.Owner
-
-    if self.Last ~= self.Owner then
-        self.Last = self.Owner
-    end
+    local own = self:GetOwner()
 
     -- spooky
     if (not IsValid(self.Ghost) or self.Ghost:GetModel() ~= own:GetModel()) and CLIENT then
@@ -297,56 +286,32 @@ function SWEP:Think()
     end
 
     -- mechanics
-    if own:KeyDown(IN_ATTACK) and self.Charge > 0 then
-        if self.Active == -1 then
-            self.Active = CurTime()
-
-            if CLIENT then
-                self.Ghost:SetSequence(own:GetSequence())
-            end
-
-            self:EmitSound(aim[math.random(1, 2)])
-        end
-    elseif self.Active > 0 then
-        self.Active = -1
-        own.BlinkImmunity = CurTime() + 2
-        local label = "Recharge" .. self:EntIndex()
-
-        if timer.Exists(label) then
-            timer.Destroy(label)
+    if (self:GetAttacking() and not own:KeyDown(IN_ATTACK)) then
+        if self.FOV then
+            own:SetFOV(self.FOV, 0)
         end
 
-        timer.Create(label, 0.02, 0, function()
-            if not IsValid(self) then
-                timer.Remove(label)
+        self.FOV = own:GetFOV()
+        local pos, norm = self:Trace()
+        own:SetFOV(self.FOV * 1.25, 0)
+        own:SetFOV(self.FOV, 1)
 
-                return
-            end
-
-            if self.Charge < self.MaxCharge then
-                self.Charge = math.min(self.MaxCharge, self.Charge + 0.2)
-            else
-                timer.Remove(label)
-            end
-        end)
-
-        timer.Start(label)
-
-        if SERVER then
-            if self.FOV then
-                own:SetFOV(self.FOV, 0)
-            end
-
-            self.FOV = own:GetFOV()
-            local pos, norm = self:Trace()
-            own:SetFOV(self.FOV * 1.25, 0)
-            own:SetFOV(self.FOV, 1)
-            if (cur_random_round and cur_random_round ~= "Invisible Traitors") then
-                own:DrawShadow(false)
-                own:SetRenderMode(RENDERMODE_TRANSALPHA)
+        if (cur_random_round and cur_random_round ~= "Invisible Traitors") then
+            own:DrawShadow(false)
+            own:SetRenderMode(RENDERMODE_TRANSALPHA)
+            if (SERVER) then
                 own:Fire("alpha", 0, 0)
             end
-            self.BlinkInfo = {pos, CurTime() + 2}
+        end
+
+        self:SetBlinkPos(pos)
+        self:SetOriginPos(own:GetPos())
+        self:SetBlinkTime(CurTime())
+        self:SetBlinking(true)
+        self:SetAttacking(false)
+        self:SetImmunityTime(CurTime() + 2)
+
+        if (SERVER) then
             RagdollDissolveEffect(own)
         else
             local vm = own:GetViewModel()
@@ -356,40 +321,33 @@ function SWEP:Think()
             end
         end
 
-        if SERVER or IsFirstTimePredicted() then
-            local take = math.ceil(takecharge:GetInt() * takecap:GetFloat())
+        local take = math.ceil(takecharge:GetInt() * takecap:GetFloat())
 
-            if self.Charge < self.MaxCharge or self.MaxCharge <= take then
-                self.MaxCharge = math.max(0, math.floor(self.MaxCharge - take))
-            end
-
-            self.Charge = math.max(0, self.Charge - takecharge:GetInt())
+        if self:GetCharge() < self:GetMaxCharge() or self:GetMaxCharge() <= take then
+            self:SetMaxCharge(math.max(0, math.floor(self:GetCharge())))
         end
+
+        self:SetCharge(math.max(0, self:GetCharge() - takecharge:GetInt()))
 
         self:EmitSound(tpl[math.random(1, 2)])
-    elseif self.Active == -2 then
-        self.Active = -1
     end
+end
 
-    if self.BlinkInfo then
-        if CurTime() >= self.BlinkInfo[2] then
+function SWEP:FinishMove(ply, mv)
+    local own = self:GetOwner()
+    if (ply ~= own) then
+        return
+    end
+    if (self:GetBlinking()) then
+        local ratio = math.min(1, (CurTime() - self:GetBlinkTime()) / (self:GetBlinkPos():Distance(self:GetOriginPos()) / 5000))
+
+        mv:SetOrigin(self:GetOriginPos() + (self:GetBlinkPos() - self:GetOriginPos()) * ratio)
+
+        if (ratio == 1) then
             self:Uncloak(own)
-            self.BlinkInfo = false
-
-            return
+            self:SetBlinking(false)
+            mv:SetVelocity(vector_origin)
         end
-
-        if own:GetPos():Distance(self.BlinkInfo[1]) <= 28 then
-            self:Uncloak(own)
-            own:SetVelocity(-own:GetVelocity() * 0.95)
-            own:ViewPunch(Angle(2, 0, 0))
-            self.BlinkInfo = false
-
-            return
-        end
-
-        own:SetVelocity((self.BlinkInfo[1] - own:GetPos()):GetNormalized() * 3182 - own:GetVelocity())
-        --print( self.Charge )
     end
 end
 
@@ -397,7 +355,7 @@ end
 function SWEP:ViewModelDrawn()
     render.SetBlend(1)
 
-    if self.Active > 0 then
+    if self:GetAttacking() then
         local pos, _ = self:Trace()
         self.Ghost:SetPos(pos)
         self.Ghost:SetAngles(self.Owner:GetAngles())
@@ -410,8 +368,8 @@ function SWEP:DrawHUD()
     local y = ScrH() / 2.0
     y = y + (y / 3)
     local ccvar = maxcharge:GetInt()
-    local charge = self.Charge / ccvar
-    local chargem = self.MaxCharge / ccvar
+    local charge = self:GetCharge() / ccvar
+    local chargem = self:GetMaxCharge() / ccvar
     local w, h = 200, 20
 
     if chargem > 0 then
@@ -435,10 +393,11 @@ function SWEP:DrawHUD()
     self.BaseClass.DrawHUD(self)
 end
 
-hook.Remove("EntityTakeDamage", "BlinkFixCrush")
-
 hook.Add("EntityTakeDamage", "BlinkFixCrush", function(ent, dmg)
-    if IsValid(ent) and ent:IsPlayer() and ent.BlinkImmunity and dmg:IsFallDamage() then
-        dmg:ScaleDamage(0)
+    if IsValid(ent) and ent:IsPlayer() and dmg:IsFallDamage() then
+        local wep = ent:GetWeapon "weapon_vadim_blink"
+        if (IsValid(wep) and wep:GetImmunityTime() > CurTime()) then
+            dmg:ScaleDamage(0)
+        end
     end
 end)
