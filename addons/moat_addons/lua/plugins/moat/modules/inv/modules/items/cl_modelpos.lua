ed53cbfde86ebd12a37781dc53971f55aed94dc9
Velkon <velkongmod@gmail.com>
2018-10-29 23:51:22 -0600

    Fix unused local

--- addons/moat_addons/lua/plugins/moat/modules/inv/modules/items/cl_modelpos.lua
+++ addons/moat_addons/lua/plugins/moat/modules/inv/modules/items/cl_modelpos.lua
@@ -215,8 +215,6 @@ timer.Simple(5, function()
     end)*/
 end)
 
-local max_rounds = GetConVarNumber("ttt_round_limit")
-
 hook.Add("TTTBeginRound", "moat_NewRoundNotify", function()
     local cur_round = (GetConVarNumber("ttt_round_limit")-GetGlobalInt("ttt_rounds_left")) + 1
     chat.AddText(Material("icon16/information.png"), Color(0, 255, 255), "Round ",Color(255,255,255), "#" .. cur_round, Color(0, 255, 255), " has begun!")
