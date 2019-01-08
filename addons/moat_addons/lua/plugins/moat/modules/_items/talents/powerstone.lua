1e61597f2b1670b06143df38d1a21cbc46f8f218
Meepen <meepdarknessmeep@gmail.com>
2019-01-08 09:46:42 -0500

    Change Power Stone to be 1-5% chance, fix some internal issues with power stone not procing at correct percents

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/powerstone.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/powerstone.lua
@@ -5,14 +5,14 @@ TALENT.Description = "Each shot has a %s_^ chance to deal double damage"
 TALENT.Tier = 3
 TALENT.LevelRequired = {min = 25, max = 30}
 TALENT.Modifications = {}
-TALENT.Modifications[1] = {min = 1, max = 4}
+TALENT.Modifications[1] = {min = 1, max = 5}
 TALENT.Melee = true
 TALENT.NotUnique = false
 
 function TALENT:OnPlayerHit(victim, attacker, dmginfo, talent_mods )
 	if (GetRoundState() ~= ROUND_ACTIVE or victim:HasGodMode()) then return end
 	local chance = self.Modifications[1].min + ( ( self.Modifications[1].max - self.Modifications[1].min ) * talent_mods[1] )
-	local random_num = math.Rand(1, 100)
+	local random_num = math.random() * 100
 	local apply_mod = chance > random_num
 	if (apply_mod) then
         dmginfo:ScaleDamage(2)
