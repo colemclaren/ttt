633bea8e64080d7a7498c5d54a4e6fc334f02340
moat <moat@moat.gg>
2019-01-13 01:38:41 -0800

    : Test hitreg ping changes to see if anyone really has a problem with it lmaooo

--- addons/moat_addons/lua/plugins/moat/modules/hit/sv_ahr.lua
+++ addons/moat_addons/lua/plugins/moat/modules/hit/sv_ahr.lua
@@ -44,7 +44,7 @@ end)
 
 local ENTITY = FindMetaTable "Entity"
 function ENTITY:HitRegCheck()
-	return self:IsPlayer() and self:GetInfoNum("moat_alt_hitreg", 1) == 1 and self:Ping() < MAX_Ping
+	return self:IsPlayer() and self:GetInfoNum("moat_alt_hitreg", 1) == 1 and self:Ping() < 300 --MAX_Ping
 end
 
 function SHR:CreateShot(wpn, shotnum, firenum)
