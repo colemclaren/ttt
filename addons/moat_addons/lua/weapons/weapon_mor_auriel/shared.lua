253561b704c17e238189e4a1579eb14cfd4fb79b
Ling <lingbleed@gmail.com>
2019-01-14 20:01:21 +0100

    Several bow fixes

--- addons/moat_addons/lua/weapons/weapon_mor_auriel/shared.lua
+++ addons/moat_addons/lua/weapons/weapon_mor_auriel/shared.lua
@@ -1,33 +1,17 @@
-if( SERVER ) then
-	AddCSLuaFile( "shared.lua" )
-end
+
+AddCSLuaFile()
 
 SWEP.PrintName = "Auriel Bow"
-if( CLIENT ) then
-	SWEP.DrawAmmo = true
-	SWEP.DrawCrosshair = false
-	SWEP.Icon = "vgui/ttt/icon_scout"
-end
 
 SWEP.Base			= "weapon_mor_base_bow"
-
-SWEP.ViewModelFOV	= 72
-SWEP.ViewModelFlip	= true
 
 SWEP.ViewModel      = "models/morrowind/ariel/longbow/v_ariel_longbow.mdl"
 SWEP.WorldModel   = "models/morrowind/ariel/longbow/w_ariel_longbow.mdl"
 
 SWEP.Primary.Damage		= 100
-SWEP.Primary.Velocity 		= 3000
+SWEP.Primary.Velocity	= 3000
 
-SWEP.Primary.ClipSize = 5
-SWEP.Primary.ClipMax = 15 -- keep mirrored to ammo
-SWEP.Primary.DefaultClip = 5
-SWEP.Primary.Automatic		= true				// Automatic/Semi Auto
-SWEP.Primary.Ammo			= "357"
-SWEP.Crosshair				= true
-SWEP.AmmoEnt = "item_ammo_357_ttt"
-SWEP.Secondary.ClipSize		= -1					// Size of a clip
-SWEP.Secondary.DefaultClip	= -1					// Default number of bullets in a clip
-SWEP.Secondary.Automatic	= false				// Automatic/Semi Auto
-SWEP.Secondary.Ammo		= "none"
+SWEP.Primary.ClipSize		= 5
+SWEP.Primary.ClipMax		= 15
+SWEP.Primary.DefaultClip	= 5
+--SWEP.Primary.Automatic	= false				// Automatic/Semi Auto
