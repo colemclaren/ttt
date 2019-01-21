de9a75b47eed6c6c954c8e76b327762f14526a91
Meepen <meepdarknessmeep@gmail.com>
2019-01-20 22:19:01 -0500

    fix up code styles in some talents

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/spacestone.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/spacestone.lua
@@ -14,13 +14,13 @@ TALENT.Melee = true
 TALENT.NotUnique = false
 
 function TALENT:OnPlayerDeath(vic, inf, att, talent_mods)
-    if (GetRoundState() ~= ROUND_ACTIVE or MOAT_ACTIVE_BOSS) then return end
+	if (GetRoundState() ~= ROUND_ACTIVE or MOAT_ACTIVE_BOSS) then return end
 
-    local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
-    if (chance > math.random() * 100) then
-        local sec = self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2])
-        status.Inflict("Space Stone", {Time = sec, Player = att})
-    end
+	local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
+	if (chance > math.random() * 100) then
+		local sec = self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2])
+		status.Inflict("Space Stone", {Time = sec, Player = att})
+	end
 end
 
 local STATUS = status.Create "Space Stone"
@@ -40,13 +40,13 @@ EFFECT.Material = "icon16/arrow_up.png"
 function EFFECT:Init(data)
 	local att = data.Player
 	att:SetGravity(0.25)
-	
+
 	self:CreateEndTimer(data.Time, data)
 end
 
 function EFFECT:OnEnd(data)
 	if (not IsValid(data.Player)) then return end
-	
+
 	local att = data.Player
 	att:SetGravity(1)
 end
