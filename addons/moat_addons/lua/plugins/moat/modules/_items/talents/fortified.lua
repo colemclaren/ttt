428ff6979fcc0850bf648e111764af56b8379ce9
Ling <lingbleed@gmail.com>
2019-02-14 19:18:04 +0100

    :Update fortified values

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/fortified.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/fortified.lua
@@ -2,22 +2,26 @@
 TALENT.ID = 26
 TALENT.Name = 'Fortified'
 TALENT.NameColor = Color(200, 200, 200)
-TALENT.Description = 'After killing a player, you receive %s_^ damage reduction for %s^ seconds'
+TALENT.Description = 'After killing a player, you have a %s_^ chance to receive a %s_^ damage reduction for %s^ seconds'
 TALENT.Tier = 2
 TALENT.LevelRequired = {min = 15, max = 20}
 
 TALENT.Modifications = {}
-TALENT.Modifications[1] = {min = 5, max = 20} -- Percent damage reduction
-TALENT.Modifications[2] = {min = 5, max = 20} -- Duration
+TALENT.Modifications[1] = {min = 50, max = 80} -- Percent damage reduction
+TALENT.Modifications[2] = {min = 5,  max = 10} -- Percent damage reduction
+TALENT.Modifications[3] = {min = 5,  max = 20} -- Duration
 
 TALENT.Melee = true
 TALENT.NotUnique = true
 
 function TALENT:OnPlayerDeath(vic, inf, att, talent_mods)
-	local percent = (self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])) / 100
-	local duration = self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2])
+	local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
+	if (chance > math.random() * 100) then
+		local percent = (self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2])) / 100
+		local duration = self.Modifications[3].min + ((self.Modifications[3].max - self.Modifications[3].min) * talent_mods[3])
 
-	status.Inflict("Fortified", {Time = duration, Player = att, Percent = percent})
+		status.Inflict("Fortified", {Time = duration, Player = att, Percent = percent})
+	end
 end
 
 local STATUS = status.Create "Fortified"
