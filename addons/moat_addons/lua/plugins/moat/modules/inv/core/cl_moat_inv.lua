14189686f2b3405626fe8f07325f4079a71b5b55
Velkon <velkongmod@gmail.com>
2019-03-05 18:22:25 -0700

    Change title of imgur uploads to avoid ||spoilers|| on mobile discord (lol)

--- addons/moat_addons/lua/plugins/moat/modules/inv/core/cl_moat_inv.lua
+++ addons/moat_addons/lua/plugins/moat/modules/inv/core/cl_moat_inv.lua
@@ -5025,7 +5025,7 @@ function m_CreateItemMenu(num, ldt)
                     MOAT_UPLOADING = false
                 end,
                 parameters = {
-                    title = (item_name) .. " || " .. LocalPlayer():Nick() .. " || Click here for more info",
+                    title = (item_name) .. " / " .. LocalPlayer():Nick() .. " / Click here for more info",
                     description = (item_name) .. "\nOwned by " .. LocalPlayer():Nick() .. " (" .. LocalPlayer():SteamID() .. ") (https://steamcommunity.com/profiles/" .. LocalPlayer():SteamID64() .. ")\nCaptured on " .. (GetServerName() or "moat.gg") .. "\n\nCheck out our website at https://moat.gg/\nOr #trading-chat in our Discord at http://discord.gg/moatgaming!"
                 },
             })
