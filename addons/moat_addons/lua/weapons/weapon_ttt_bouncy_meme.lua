c06d80103b229fb515ca055126ad947b3256d63f
Ling <lingbleed@gmail.com>
2019-03-18 08:21:49 +0100

    Nocollides memeballs and ups the remove time to 30 seconds

--- addons/moat_addons/lua/weapons/weapon_ttt_bouncy_meme.lua
+++ addons/moat_addons/lua/weapons/weapon_ttt_bouncy_meme.lua
@@ -67,11 +67,12 @@ function SWEP:PrimaryAttack()
         ent:SetBallSize(size)
         ent:Spawn()
         ent:Activate()
-        ent:GetPhysicsObject():SetVelocity(owner:GetAimVector() * 600 + owner:GetVelocity())
+        ent:SetVelocity(owner:GetAimVector() * 600 + owner:GetVelocity())
+		ent:SetCollisionGroup(COLLISION_GROUP_NONE)
         ent.OnRemove = function(s) self.Balls = self.Balls - 1 end
         ent.Use = function(s) s:Remove() end
 		
-		timer.Simple(10, function()
+		timer.Simple(30, function()
 			if (IsValid(ent)) then
 				ent:Remove()
 			end
