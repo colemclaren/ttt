649a14fa110e5df5dd51bf8808bac77e9dc09042
Velkon <velkongmod@gmail.com>
2019-03-29 02:38:47 -0600

    Change silenced talent description

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/silenced.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/silenced.lua
@@ -2,7 +2,7 @@
 TALENT.ID = 9040
 TALENT.Name = "Silenced"
 TALENT.NameColor = Color(0, 255,0)
-TALENT.Description = "Each round, your weapon has a %s_^ chance to become silenced"
+TALENT.Description = "Your weapons is always silenced"
 TALENT.Tier = 1
 TALENT.LevelRequired = {min = 5, max = 10}
 
@@ -22,28 +22,9 @@ end)
 local silencedSound = Sound("weapons/usp/usp1.wav")
 
 function TALENT:ModifyWeapon( weapon, talent_mods )
-	local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
-	local applyMod = chance > math.random() * 100
+	net.Start("Talents.Silenced")
+		net.WriteEntity(_weapon)
+	net.Broadcast()
 
-	local _weapon = weapon.Weapon
-	local _owner = _weapon:GetOwner()
-	local _class = _weapon:GetClass()
-
-	if not silence_prep_cache[_owner] then
-		silence_prep_cache[_owner] = {}
-	end
-
-	if silence_prep_cache[_owner][_class] then
-		applyMod = silence_prep_cache[_owner][_class]
-	else
-		silence_prep_cache[_owner][_class] = applyMod
-	end
-
-	if (applyMod) then
-		net.Start("Talents.Silenced")
-			net.WriteEntity(_weapon)
-		net.Broadcast()
-
-		_weapon.Primary.Sound = silencedSound
-	end
+	_weapon.Primary.Sound = silencedSound
 end
