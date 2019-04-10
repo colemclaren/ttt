91f5f26239851b32917a80877d39687dd6de0d63
Meepen <meepdarknessmeep@gmail.com>
2019-04-10 19:07:02 -0400

    Make 9mm canisters have 30 bullets in it

--- gamemodes/terrorcity/entities/entities/item_ammo_pistol_ttt.lua
+++ gamemodes/terrorcity/entities/entities/item_ammo_pistol_ttt.lua
@@ -1,11 +1,22 @@
--- Pistol ammo override
-
-AddCSLuaFile()
-
-ENT.Type = "anim"
-ENT.Base = "base_ammo_ttt"
-ENT.AmmoType = "Pistol"
-ENT.AmmoAmount = 20
-ENT.AmmoMax = 60
-ENT.Model = Model("models/items/boxsrounds.mdl")
-ENT.AutoSpawnable = true
+-- Pistol ammo override
+
+
+
+AddCSLuaFile()
+
+
+
+ENT.Type = "anim"
+
+ENT.Base = "base_ammo_ttt"
+
+ENT.AmmoType = "Pistol"
+
+ENT.AmmoAmount = 30
+
+ENT.AmmoMax = 60
+
+ENT.Model = Model("models/items/boxsrounds.mdl")
+
+ENT.AutoSpawnable = true
+
