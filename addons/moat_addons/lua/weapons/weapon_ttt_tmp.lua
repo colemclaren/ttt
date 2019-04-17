492abd0ba7d4c52f5f5000d84c0f24f18faa7eeb
Meepen <meepdarknessmeep@gmail.com>
2019-04-17 07:41:36 -0400

    Delay for TMP reduced from 0.04 to 0.035, mag size inreased to 40

--- addons/moat_addons/lua/weapons/weapon_ttt_tmp.lua
+++ addons/moat_addons/lua/weapons/weapon_ttt_tmp.lua
@@ -11,11 +11,11 @@ SWEP.Kind = WEAPON_HEAVY
 SWEP.ENUM = 7
 
 SWEP.Primary.Damage      = 8
-SWEP.Primary.Delay       = 0.04
+SWEP.Primary.Delay       = 0.035
 SWEP.Primary.Cone        = 0.025
-SWEP.Primary.ClipSize    = 36
+SWEP.Primary.ClipSize    = 40
 SWEP.Primary.ClipMax     = 60
-SWEP.Primary.DefaultClip = 36
+SWEP.Primary.DefaultClip = 40
 SWEP.Primary.Automatic   = true
 SWEP.Primary.Ammo        = "pistol"
 SWEP.Primary.Recoil      = 1.2
