74319c2888ed82ed3f6d0043b341f8b8fe90aef7
Meepen <meepdarknessmeep@gmail.com>
2019-04-20 02:30:06 -0400

    Fix dual weapons causing errors if you join mid round

--- addons/moat_addons/lua/weapons/weapon_ttt_dual_glock.lua
+++ addons/moat_addons/lua/weapons/weapon_ttt_dual_glock.lua
@@ -40,21 +40,11 @@ SWEP.Primary.Sound = Sound "Weapon_Glock.Single"
 
 SWEP.HeadshotMultiplier = 1.45
 
+if (SERVER) then
+    return
+end
+
 DEFINE_BASECLASS "weapon_tttbase"
-
-function SWEP:Initialize()
-    BaseClass.Initialize(self)
-    if (CLIENT) then
-        self.WorldModelEntity = ClientsideModel(self.WorldModel)
-        self.WorldModelEntity:SetNoDraw(true)
-    end
-end
-
-function SWEP:OnRemove()
-    if (CLIENT) then
-        self.WorldModelEntity:Remove()
-    end
-end
 
 function SWEP:ViewModelDrawn(vm)
     if (self.InsideDraw) then
@@ -71,92 +61,110 @@ function SWEP:ViewModelDrawn(vm)
     self.InsideDraw = false
 end
 
-if (CLIENT) then
-    function SWEP:DrawWorldModel()
-        local WorldModel = self.WorldModelEntity
-        local _Owner = self:GetOwner()
-    
-        local lp = LocalPlayer()
-        if (lp ~= _Owner) then
-            if (lp:GetObserverMode() == OBS_MODE_IN_EYE and lp:GetObserverTarget() == _Owner) then
-                return
-            end
+function SWEP:GetWorldModel2()
+    if (IsValid(self.WorldModelEntity)) then
+        return self.WorldModelEntity
+    end
+    self.WorldModelEntity = ClientsideModel(self.WorldModel)
+    if (IsValid(self.WorldModelEntity)) then
+        self.WorldModelEntity:SetNoDraw(true)
+        return self.WorldModelEntity
+    end
+end
+
+function SWEP:OnRemove()
+    if (CLIENT) then
+        self:GetWorldModel2():Remove()
+    end
+end
+
+function SWEP:DrawWorldModel()
+    local WorldModel = self:GetWorldModel2()
+    if (not IsValid(WorldModel)) then
+        return
+    end
+    local _Owner = self:GetOwner()
+
+    local lp = LocalPlayer()
+    if (lp ~= _Owner) then
+        if (lp:GetObserverMode() == OBS_MODE_IN_EYE and lp:GetObserverTarget() == _Owner) then
+            return
         end
+    end
+
+    if (old) then
+        old(self)
+    else
+        self:DrawModel()
+    end
     
-        if (old) then
-            old(self)
-        else
-            self:DrawModel()
-        end
+    if (IsValid(_Owner)) then
+        -- Specify a good position
         
-        if (IsValid(_Owner)) then
-            -- Specify a good position
-            
-            _Owner:SetupBones()
-            local boneid = _Owner:LookupBone "ValveBiped.Bip01_L_Hand" -- Right Hand
-            local b3 = WorldModel:LookupBone "ValveBiped.Bip01_R_Hand" or WorldModel:LookupBone "ValveBiped.weapon_bone"
-            if not boneid or not b3 then return end
+        _Owner:SetupBones()
+        local boneid = _Owner:LookupBone "ValveBiped.Bip01_L_Hand" -- Right Hand
+        local b3 = WorldModel:LookupBone "ValveBiped.Bip01_R_Hand" or WorldModel:LookupBone "ValveBiped.weapon_bone"
+        if not boneid or not b3 then return end
 
-    
-            local matrix = _Owner:GetBoneMatrix(boneid)
-            local mpos, mang
-            local m3 = WorldModel:GetBoneMatrix(b3)
-            if (m3) then
-                mpos, mang = m3:GetTranslation(), m3:GetAngles()
-            else
-                mpos, mang = WorldModel:GetBonePosition(b3)
-            end
-    
-            if not matrix or not mpos then return end
-            
-            local pos, ang = LocalToWorld(vector_origin, Angle(0, 0, 180), matrix:GetTranslation(), matrix:GetAngles())
-            local lpos, lang = WorldToLocal(WorldModel:GetPos(), WorldModel:GetAngles(), mpos, mang)
-            pos, ang = LocalToWorld(lpos, lang, pos, ang)
-    
-            if (self.Offset) then
-                pos = pos + ang:Forward() * self.Offset.Pos.Forward + ang:Right() * self.Offset.Pos.Right + ang:Up() * self.Offset.Pos.Up
-                ang:RotateAroundAxis(ang:Up(), self.Offset.Ang.Up)
-                ang:RotateAroundAxis(ang:Right(), self.Offset.Ang.Right)
-                ang:RotateAroundAxis(ang:Forward(), self.Offset.Ang.Forward)
-                ang:RotateAroundAxis(ang:Forward(), 180)
-            end
-    
-            local offsetVec = self.OffsetVector or Vector(0, -2, 0)
-            local offsetAng = self.OffsetAngle or Angle(-5, -10, 0)
-    
-            pos, ang = LocalToWorld(offsetVec, offsetAng, pos, ang)
-    
-            WorldModel:SetPos(pos)
-            WorldModel:SetAngles(ang)
-            WorldModel:SetupBones()
+
+        local matrix = _Owner:GetBoneMatrix(boneid)
+        local mpos, mang
+        local m3 = WorldModel:GetBoneMatrix(b3)
+        if (m3) then
+            mpos, mang = m3:GetTranslation(), m3:GetAngles()
         else
-            WorldModel:SetPos(self:GetPos() + self:GetAngles():Right() * 5)
-            WorldModel:SetAngles(self:GetAngles())
-        end
-        WorldModel:DrawModel()
-    end
-
-    function SWEP:FireAnimationEvent( pos, ang, event, options )
-        if (not self.CSMuzzleFlashes) then return end
-        if (not IsValid(self.Owner)) then return end
-
-        -- CS Muzzle flashes
-        if ( event == 5001 or event == 5011 or event == 5021 or event == 5031 ) then
-
-            local data = EffectData()
-            data:SetFlags( 0 )
-            data:SetEntity( self.Owner:GetViewModel() )
-            data:SetAttachment( math.floor( ( event - 4991 ) / 10 ) )
-            data:SetScale( 1 )
-
-            if ( self.CSMuzzleX ) then
-                util.Effect( "CS_MuzzleFlash_X", data )
-            else
-                util.Effect( "CS_MuzzleFlash", data )
-            end
-
-            return true
+            mpos, mang = WorldModel:GetBonePosition(b3)
         end
 
+        if not matrix or not mpos then return end
+        
+        local pos, ang = LocalToWorld(vector_origin, Angle(0, 0, 180), matrix:GetTranslation(), matrix:GetAngles())
+        local lpos, lang = WorldToLocal(WorldModel:GetPos(), WorldModel:GetAngles(), mpos, mang)
+        pos, ang = LocalToWorld(lpos, lang, pos, ang)
+
+        if (self.Offset) then
+            pos = pos + ang:Forward() * self.Offset.Pos.Forward + ang:Right() * self.Offset.Pos.Right + ang:Up() * self.Offset.Pos.Up
+            ang:RotateAroundAxis(ang:Up(), self.Offset.Ang.Up)
+            ang:RotateAroundAxis(ang:Right(), self.Offset.Ang.Right)
+            ang:RotateAroundAxis(ang:Forward(), self.Offset.Ang.Forward)
+            ang:RotateAroundAxis(ang:Forward(), 180)
+        end
+
+        local offsetVec = self.OffsetVector or Vector(0, -2, 0)
+        local offsetAng = self.OffsetAngle or Angle(-5, -10, 0)
+
+        pos, ang = LocalToWorld(offsetVec, offsetAng, pos, ang)
+
+        WorldModel:SetPos(pos)
+        WorldModel:SetAngles(ang)
+        WorldModel:SetupBones()
+    else
+        WorldModel:SetPos(self:GetPos() + self:GetAngles():Right() * 5)
+        WorldModel:SetAngles(self:GetAngles())
     end
+    WorldModel:DrawModel()
+end
+
+function SWEP:FireAnimationEvent( pos, ang, event, options )
+    if (not self.CSMuzzleFlashes) then return end
+    if (not IsValid(self.Owner)) then return end
+
+    -- CS Muzzle flashes
+    if ( event == 5001 or event == 5011 or event == 5021 or event == 5031 ) then
+
+        local data = EffectData()
+        data:SetFlags( 0 )
+        data:SetEntity( self.Owner:GetViewModel() )
+        data:SetAttachment( math.floor( ( event - 4991 ) / 10 ) )
+        data:SetScale( 1 )
+
+        if ( self.CSMuzzleX ) then
+            util.Effect( "CS_MuzzleFlash_X", data )
+        else
+            util.Effect( "CS_MuzzleFlash", data )
+        end
+
+        return true
+    end
+
 end
