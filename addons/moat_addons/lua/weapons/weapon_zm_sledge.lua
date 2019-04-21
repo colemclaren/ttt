0cd28de7c072fa723cd214964276cb6825bfa343
Meepen <meepdarknessmeep@gmail.com>
2019-04-21 13:26:55 -0400

    Change HUGE-249 - Damage from 11 to 9, Spread from 0.09 to 0.05, Recoil from 1.9 to 1.4, Range to 500

--- addons/moat_addons/lua/weapons/weapon_zm_sledge.lua
+++ addons/moat_addons/lua/weapons/weapon_zm_sledge.lua
@@ -21,17 +21,18 @@ SWEP.Kind = WEAPON_HEAVY
 SWEP.WeaponID = AMMO_M249
 SWEP.ENUM = 1
 
-SWEP.Primary.Damage = 11
+SWEP.Primary.Damage         = 8
 SWEP.Primary.Delay = 0.06
-SWEP.Primary.Cone = 0.09
+SWEP.Primary.Cone           = 0.05
 SWEP.Primary.ClipSize = 150
 SWEP.Primary.ClipMax = 150
 SWEP.Primary.DefaultClip	= 150
 SWEP.Primary.Automatic		= true
 SWEP.Primary.Ammo			= "AirboatGun"
 SWEP.AutoSpawnable      = true
-SWEP.Primary.Recoil			= 1.9
+SWEP.Primary.Recoil			= 1.4
 SWEP.Primary.Sound			= Sound("Weapon_m249.Single")
+SWEP.Primary.Range          = 500
 
 SWEP.UseHands			= true
 SWEP.ViewModelFlip		= false
