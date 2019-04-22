bc10be38fd41567ef963be06f980ef48c01303ec
Ling <lingbleed@gmail.com>
2019-04-22 17:34:20 +0200

    Makes Scavenger give ammo directly instead of dropping it

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/scavenger.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/scavenger.lua
@@ -2,13 +2,13 @@
 TALENT.ID = 82
 TALENT.Name = "Scavenger"
 TALENT.NameColor = Color(178, 102, 255)
-TALENT.Description = "Players have a %s_^ chance to drop %s primary ammo (or until filled) if killed with this weapon"
+TALENT.Description = "Players have a %s_^ chance to receive %s ammo after killing a target"
 TALENT.Tier = 2
 TALENT.LevelRequired = {min = 15, max = 20}
 
 TALENT.Modifications = {}
 TALENT.Modifications[1] = {min = 20, max = 50}	-- Chance to trigger
-TALENT.Modifications[2] = {min = 5 , max = 30}	-- Amount of primary ammo to drop
+TALENT.Modifications[2] = {min = 5 , max = 30}	-- Amount of ammo to give
 
 TALENT.Melee = false
 TALENT.NotUnique = true
@@ -16,22 +16,12 @@ TALENT.NotUnique = true
 function TALENT:OnPlayerDeath(vic, inf, att, talent_mods)
     local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
     if (chance > math.random() * 100) then
-    	local ammo = math.Round(self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2]))
-        local ammo_class = inf.AmmoEnt or "item_ammo_smg1_ttt"
-        local ammo_pos = vic:GetPos()
-        local ammo_ent = ents.Create(ammo_class)
-        if (IsValid(ammo_ent)) then
-            ammo_pos.z = ammo_pos.z + 5
-            ammo_ent.AmmoAmount = ammo
-            ammo_ent:SetPos(ammo_pos)
-            ammo_ent:SetAngles(VectorRand():Angle())
-            ammo_ent:Spawn()
-            ammo_ent:PhysWake()
+        local ammo = math.Round(self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2]))
+        inf:SetClip1(inf:Clip1() + ammo)
 
-            net.Start("Moat.Talents.Notify")
-                net.WriteUInt(1, 8)
-                net.WriteString("Scavenger activated on kill!")
-            net.Send(att)
-        end
+        net.Start("Moat.Talents.Notify")
+            net.WriteUInt(1, 8)
+            net.WriteString("Scavenger activated on kill!")
+        net.Send(att)
     end
 end
