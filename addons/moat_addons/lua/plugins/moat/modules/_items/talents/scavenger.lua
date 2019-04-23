2a6b4cccb6b321c82428f4696abeeb07098f7b32
Ling <lingbleed@gmail.com>
2019-04-23 20:55:27 +0200

    Fix scavenger

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/scavenger.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/scavenger.lua
@@ -17,7 +17,7 @@ function TALENT:OnPlayerDeath(vic, inf, att, talent_mods)
     local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
     if (chance > math.random() * 100) then
         local ammo = math.Round(self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2]))
-        inf:SetClip1(inf:Clip1() + ammo)
+        att:GiveAmmo(ammo, inf:GetPrimaryAmmoType(), true)
 
         net.Start("Moat.Talents.Notify")
             net.WriteUInt(1, 8)
