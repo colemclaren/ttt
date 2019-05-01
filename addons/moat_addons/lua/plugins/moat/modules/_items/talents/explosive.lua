33296db15ab109121282da008e69d6b6ac69987e
Meepen <meepdarknessmeep@gmail.com>
2019-05-01 08:39:26 -0400

    Fix Explosive chance for pellets > 1

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/explosive.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/explosive.lua
@@ -1,13 +1,12 @@
-
 TALENT.ID = 87
 TALENT.Name = "Explosive"
 TALENT.NameColor = Color(255, 128, 0)
-TALENT.Description = "Every quarter second of firing, this gun has a %s_^ chance to fire an explosive round dealing %s damage"
+TALENT.Description = "Every second of firing, this gun has a %s_^ chance to fire an explosive round dealing %s damage"
 TALENT.Tier = 2
 TALENT.LevelRequired = {min = 15, max = 20}
 
 TALENT.Modifications = {}
-TALENT.Modifications[1] = {min = 21   , max = 40}	-- Chance to trigger
+TALENT.Modifications[1] = {min = 84   , max = 120}	-- Chance to trigger
 TALENT.Modifications[2] = {min = 13.37, max = 42}	-- Explosion damage
 
 TALENT.Melee = false
@@ -17,41 +16,44 @@ function TALENT:OnWeaponFired(attacker, wep, dmginfo, talent_mods, is_bow, hit_p
     if (not gmod.GetGamemode():AllowPVP()) then return end
     local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
 
-    if (not is_bow) then
-        if (not IsValid(wep)) then
-            return
-        end
-
-        if (not wep.Primary or not wep.Primary.Delay) then
-            attacker:PrintMessage(HUD_PRINTCENTER, "Your gun does not have valid stats for Explosive.")
-            return
-        end
-
-        local rps = (wep.Primary.NumShots or 1) / wep.Primary.Delay
-        chance = math.min(chance * 4 / rps, 25) -- 4 = quarter second
+    if (not IsValid(wep)) then
+        return
     end
 
-    if (chance > math.random() * 100) then
-    	local dmg = self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2])
+    if (not wep.Primary or not wep.Primary.Delay) then
+        attacker:PrintMessage(HUD_PRINTCENTER, "Your gun does not have valid stats for Explosive.")
+        return
+    end
 
-        if (is_bow and hit_pos) then
+    local pellets_per_sec = (wep.Primary.NumShots or 1) / wep.Primary.Delay
+    chance = chance / pellets_per_sec
+
+
+    local dmg = self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2])
+
+    if (is_bow and hit_pos) then
+        if (chance > math.random() * 100) then
             local exp = ents.Create("env_explosion")
             exp:SetOwner(attacker)
             exp:SetPos(hit_pos)
             exp:Spawn()
             exp:SetKeyValue("iMagnitude", tostring(dmg/2))
             exp:Fire("Explode", 0, 0)
-        else
-            dmginfo.Callback = function(att, tr, dmginfo)
-                if (tr.AltHitreg) then
-                    return
-                end
+        end
+    else
+        local cb = dmginfo.Callback
+        dmginfo.Callback = function(att, tr, dmginfo)
+            local rand = math.random() * 100
+            if (not tr.AltHitreg and chance > rand) then
                 local exp = ents.Create("env_explosion")
                 exp:SetOwner(attacker)
                 exp:SetPos(tr.HitPos)
                 exp:Spawn()
                 exp:SetKeyValue("iMagnitude", tostring(dmg / 2))
                 exp:Fire("Explode", 0, 0)
+            end
+            if (cb) then
+                return cb(att, tr, dmginfo)
             end
         end
     end
