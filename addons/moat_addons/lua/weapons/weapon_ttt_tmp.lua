24fc07e9e834257e16445233ad9b55ff9ea66ad4
motato <cole@moat.gg>
2019-05-22 13:58:14 -0700

    Rename TMP to just TMP and fix idle animation.

--- addons/moat_addons/lua/weapons/weapon_ttt_tmp.lua
+++ addons/moat_addons/lua/weapons/weapon_ttt_tmp.lua
@@ -2,11 +2,11 @@
 AddCSLuaFile()
 
 SWEP.HoldType = "ar2"
-SWEP.PrintName = "Steyr TMP"
+SWEP.PrintName = "TMP"
 
 SWEP.Slot = 2
 SWEP.Base = "weapon_tttbase"
-
+DEFINE_BASECLASS "weapon_tttbase"
 SWEP.Kind = WEAPON_HEAVY
 SWEP.ENUM = 7
 
@@ -31,13 +31,16 @@ SWEP.ViewModelFOV		= 54
 SWEP.ViewModel  = "models/weapons/cstrike/c_smg_tmp.mdl"
 SWEP.WorldModel = "models/weapons/w_smg_tmp.mdl"
 
-SWEP.DeploySpeed = 3
+SWEP.DeploySpeed = 1
 SWEP.ReloadSpeed = 1
 SWEP.ReloadAnim = {
 	DefaultReload = {
 		Anim = "reload",
 		Time = 2.16,
 	},
+	AfterReload = {
+		Anim = "idle"
+	}
 }
 
 SWEP.HeadshotMultiplier = 1.4
