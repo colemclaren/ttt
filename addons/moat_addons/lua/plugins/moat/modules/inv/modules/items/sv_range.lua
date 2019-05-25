3f3d9ed7c09c69f9637ed838e315a02beed4f570
Meepen <meepdarknessmeep@gmail.com>
2019-05-24 21:13:02 -0400

    Change Range file for extensive weapon overriding

--- addons/moat_addons/lua/plugins/moat/modules/inv/modules/items/sv_range.lua
+++ addons/moat_addons/lua/plugins/moat/modules/inv/modules/items/sv_range.lua
@@ -15,21 +15,21 @@ hook.Add("EntityTakeDamage", "moat_ApplyRange", function(ent, dmginfo)
         local distance = attackerpos:Distance(entpos)
         local range_mod = weapon_tbl.range_mod or 1
 
-        local optimal_range
+        local optimal_range = weapon:GetCurrentRange()
 
-        if (weapon_tbl.Primary.Range) then
-            optimal_range = weapon_tbl.Primary.Range
-        elseif (weapon_tbl.Primary.Ammo and weapon_tbl.Primary.Ammo == "Buckshot") then
-            optimal_range = 50 / weapon_tbl.Primary.Cone
-        else
-            optimal_range = RANGE_NUMBER / weapon_tbl.Primary.Cone
+        if (not optimal_range) then
+            if (weapon_tbl.Primary.Ammo and weapon_tbl.Primary.Ammo == "Buckshot") then
+                optimal_range = 50 / weapon_tbl.Primary.Cone
+            else
+                optimal_range = RANGE_NUMBER / weapon_tbl.Primary.Cone
+            end
         end
 
         optimal_range = optimal_range * range_mod
 
 
         if (distance > optimal_range) then
-            local falloff_range = (weapon_tbl.Primary.FalloffRange or optimal_range) * range_mod
+            local falloff_range = (weapon:GetCurrentMaxRange() or optimal_range) * range_mod
 
             dmginfo:ScaleDamage(math.min(1, 1 + RANGE_CAP - math.max(0, (distance - optimal_range) / falloff_range)))
         end
