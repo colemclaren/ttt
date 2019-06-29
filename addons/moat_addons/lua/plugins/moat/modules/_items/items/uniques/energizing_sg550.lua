e0b0ff9ab4d7563c8bc67009dd90e6141876d206
Meepen <meepdarknessmeep@gmail.com>
2019-06-29 00:37:47 -0400

    Change stats of some of the uniques from Summer Climb

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/energizing_sg550.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/energizing_sg550.lua
@@ -7,13 +7,13 @@ ITEM.MinStats = 6
 ITEM.MaxStats = 8
 ITEM.NameEffect = "electric"
 ITEM.Stats = {
-	Damage = { min = 17, max = 28 },
-	Accuracy = { min = 17, max = 28 },
-	Kick = { min = -17, max = -28 },
-	Firerate = { min = 17, max = 28 },
-	Magazine = { min = 23, max = 33 },
-	Range = { min = 23, max = 33 },
-	Weight = { min = -5, max = -7 },
+	Damage = { min = 14, max = 23 },
+	Accuracy = { min = 14, max = 23 },
+	Kick = { min = -14, max = -23 },
+	Firerate = { min = 22, max = 33 },
+	Magazine = { min = 19, max = 28 },
+	Range = { min = 19, max = 28 },
+	Weight = { min = -4, max = -7 },
     Deployrate = { min = 30, max = 40 },
     Reloadrate = { min = 30, max = 40 },
 }
--- addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/energizing_switchpa.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/energizing_switchpa.lua
@@ -3,16 +3,17 @@ ITEM.Name = "Zapper"
 ITEM.Rarity = 6
 ITEM.Collection = "Summer Climb Collection"
 ITEM.WeaponClass = "weapon_zapperpvp"
-ITEM.MinStats = 7
+ITEM.MinStats = 6
 ITEM.MaxStats = 7
 ITEM.NameEffect = "electric"
 ITEM.Stats = {
-	Damage = { min = 45, max = 65 },
-	Accuracy = { min = 17, max = 28 },
-	Firerate = { min = 17, max = 28 },
-	Magazine = { min = 23, max = 33 },
-	Range = { min = 23, max = 33 },
-	Weight = { min = -5, max = -7 },
+	Damage = { min = 14, max = 23 },
+	Accuracy = { min = 14, max = 23 },
+	Kick = { min = -14, max = -23 },
+	Firerate = { min = 22, max = 33 },
+	Magazine = { min = 19, max = 28 },
+	Range = { min = 19, max = 28 },
+	Weight = { min = -10, max = -15 },
 	Reloadrate = { min = 30, max = 40 },
 }
 ITEM.MinTalents = 2 
