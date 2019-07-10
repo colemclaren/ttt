598800d3ef28c60304f472f4228b236a0da27ff5
motato <cole@moat.gg>
2019-07-10 06:56:08 -0700

    TMP OP???

--- addons/moat_addons/lua/weapons/weapon_ttt_tmp.lua
+++ addons/moat_addons/lua/weapons/weapon_ttt_tmp.lua
@@ -11,12 +11,12 @@ SWEP.Kind = WEAPON_HEAVY
 SWEP.ENUM = 7
 
 SWEP.HeadshotMultiplier = 1.4
-SWEP.Primary.Damage      = 8
+SWEP.Primary.Damage      = 10
 SWEP.Primary.Delay       = 0.035
 SWEP.Primary.Cone        = 0.025
-SWEP.Primary.ClipSize    = 40
-SWEP.Primary.ClipMax     = 60
-SWEP.Primary.DefaultClip = 40
+SWEP.Primary.ClipSize    = 60
+SWEP.Primary.ClipMax     = 120
+SWEP.Primary.DefaultClip = 60
 SWEP.Primary.Automatic   = true
 SWEP.Primary.Ammo        = "pistol"
 SWEP.Primary.Recoil      = 1.2
@@ -33,7 +33,7 @@ SWEP.ViewModel  = "models/weapons/cstrike/c_smg_tmp.mdl"
 SWEP.WorldModel = "models/weapons/w_smg_tmp.mdl"
 
 SWEP.DeploySpeed = 1
-SWEP.ReloadSpeed = 1
+SWEP.ReloadSpeed = 1.2
 SWEP.ReloadAnim = {
 	DefaultReload = {
 		Anim = "reload",
