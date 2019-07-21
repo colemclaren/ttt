77f99ebd1b1402d7b7052e1636854a3e0d69f1ec
Ling <lingbleed@gmail.com>
2019-07-21 22:38:17 +0200

    Make soften actually work on hit

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/soften.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/soften.lua
@@ -14,13 +14,13 @@ TALENT.Modifications[3] = {min = 1 , max = 5 } -- Duration
 TALENT.Melee = true
 TALENT.NotUnique = true
 
-function TALENT:OnPlayerDeath(vic, inf, att, talent_mods)
+function TALENT:OnPlayerHit(vic, att, info, talent_mods)
 	local chance = self.Modifications[1].min + ((self.Modifications[1].max - self.Modifications[1].min) * talent_mods[1])
 	if (chance > math.random() * 100) then
 		local percent = (self.Modifications[2].min + ((self.Modifications[2].max - self.Modifications[2].min) * talent_mods[2])) / 100
 		local duration = self.Modifications[3].min + ((self.Modifications[3].max - self.Modifications[3].min) * talent_mods[3])
 
-		status.Inflict("Soften", {Time = duration, Player = att, Percent = percent})
+		status.Inflict("Soften", {Time = duration, Player = vic, Percent = percent})
 	end
 end
 
