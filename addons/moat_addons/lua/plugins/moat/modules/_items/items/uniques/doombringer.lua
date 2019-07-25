614cfca82e9f47b3e0243af62836d6cbcdbe1ebb
motato <cole@moat.gg>
2019-07-25 14:02:15 -0700

    Click clack we sittin' back, doom bringin' 1 to 6 taps

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/doombringer.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/doombringer.lua
@@ -1,7 +1,7 @@
 
 ITEM.ID = 316
 
-ITEM.Name = "Doom-Bringer"
+ITEM.Name = "Doom Broom"
 
 ITEM.NameColor = Color( 231, 213, 10 )
 
@@ -16,17 +16,17 @@ ITEM.MinStats = 9
 ITEM.MaxStats = 9
 
 ITEM.Stats = {
-	Damage = { min = 66, max = 86 },
-	Accuracy = { min = 15, max = 30 },
-	Kick = { min = 15, max = 32 },
-	Firerate = { min = 16, max = 28 },
+	
 	Magazine = { min = 6, max = 18 },
 	Range = { min = 10, max = 25 },
 	Weight = { min = 6, max = 18 },
     Deployrate = { min = -12, max = -25 },
     Reloadrate = { min = 15, max = 30 },
 }
-
+Damage = { min = 66, max = 86 },
+	Accuracy = { min = 15, max = 30 },
+	Kick = { min = 15, max = 32 },
+	Firerate = { min = 16, max = 28 },
 ITEM.WeaponClass = "weapon_ttt_m16"
 
 ITEM.MinTalents = 2
