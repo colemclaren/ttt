aa5354f7eed8d04f4df48c1dbfb376d1cc8c6efb
motato <cole@moat.gg>
2019-07-25 11:25:47 -0700

    What a handsome trex modwl

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/models/knight.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/models/knight.lua
@@ -1,18 +1,10 @@
-
 ITEM.ID = 545
-
 ITEM.Name = "Knight Model"
-
 ITEM.Description = "Fixed"
-
 ITEM.Model = "models/moat/player/knight_fixed.mdl"
-
 ITEM.Rarity = 6
-
 ITEM.Collection = "Model Collection"
 
 function ITEM:OnPlayerSpawn( ply )
-
-	timer.Simple( 1, function() ply:SetModel( self.Model ) end )
-
+	timer.Simple(1, function() ply:SetModel( self.Model ) end)
 end
