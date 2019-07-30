79696b715441b2cca76175a3d58f268a19fede42
Velkon <velkongmod@gmail.com>
2019-07-30 15:44:46 -0600

    Mega vape can now be used every 30 minutes

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/other/vape_mega.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/other/vape_mega.lua
@@ -1,6 +1,6 @@
 ITEM.Name = "Mega Vape"
 ITEM.ID = 909
-ITEM.Description = "An exclusive collectors item. Causes BIG smoke every 2 hours, just like a very expensive smoke grenade if you think about it"
+ITEM.Description = "An exclusive collectors item. Causes BIG smoke every 30 minutes, just like a very expensive smoke grenade if you think about it"
 ITEM.Rarity = 8
 ITEM.Image = "https://cdn.moat.gg/f/Rx0pUsUKgsDXYUzidrKNqe0L2yQS.png"
 ITEM.Collection = "Limited Collection"
--- addons/moat_addons/lua/plugins/weapons/vapes/sv_vapeswep.lua
+++ addons/moat_addons/lua/plugins/weapons/vapes/sv_vapeswep.lua
@@ -7,7 +7,7 @@ util.AddNetworkString("Vape")
 util.AddNetworkString("VapeArm")
 util.AddNetworkString("VapeTalking")
 
-local hours = 2
+local hours = 0.5
 mega_vape_cache = {}
 local function _megasql()
 	local db = MINVENTORY_MYSQL
