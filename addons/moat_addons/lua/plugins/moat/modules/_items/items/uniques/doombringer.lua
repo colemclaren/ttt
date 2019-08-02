abe3effbee5b49fda8e25a0e473c83e57bb789bd
motato <cole@moat.gg>
2019-08-02 11:25:56 -0700

    doom laden hurts less

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/doombringer.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/doombringer.lua
@@ -2,20 +2,20 @@ ITEM.ID = 316
 ITEM.Name = "Doomladen"
 ITEM.NameColor = Color(231, 213, 10)
 ITEM.NameEffect = "glow"
-ITEM.Rarity = 7
+ITEM.Rarity = 6
 ITEM.Collection = "Beta Collection"
 ITEM.WeaponClass = "weapon_ttt_m16"
 
 ITEM.MinStats = 9
 ITEM.MaxStats = 9
 ITEM.Stats = {
-	Damage = { min = 56, max = 76 },
-	Accuracy = { min = 15, max = 30 },
-	Kick = { min = 15, max = 32 },
-	Firerate = { min = 16, max = 28 },
-	Magazine = { min = 6, max = 18 },
-	Range = { min = 10, max = 25 },
-	Weight = { min = 6, max = 18 },
+    Damage = { min = 20, max = 32 },
+    Accuracy = { min = 15, max = 30 },
+    Kick = { min = 15, max = 32 },
+    Firerate = { min = 16, max = 28 },
+    Magazine = { min = 6, max = 18 },
+    Range = { min = 10, max = 25 },
+    Weight = { min = 6, max = 18 },
     Deployrate = { min = -12, max = -25 },
     Reloadrate = { min = 15, max = 30 },
 }
