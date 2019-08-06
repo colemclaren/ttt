76213288eb17ed8f3dac1a56c60dee28243f68bf
Meepen <meepdarknessmeep@gmail.com>
2019-08-05 22:02:09 -0700

    Monte Carlo guarantees 6-8 stats, Firerate nerf to 10-24, previous drops will still have 9 stats

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/beta_unique2.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/uniques/beta_unique2.lua
@@ -6,13 +6,13 @@ ITEM.Rarity = 7
 ITEM.Collection = "Beta Collection"
 ITEM.WeaponClass = "weapon_ttt_mp5"
 
-ITEM.MinStats = 9
-ITEM.MaxStats = 9
+ITEM.MinStats = 6
+ITEM.MaxStats = 8 
 ITEM.Stats = {
     Damage = { min = 17, max = 22 },
     Accuracy = { min = 17, max = 28 },
     Kick = { min = -17, max = -28 },
-    Firerate = { min = 20, max = 34 },
+    Firerate = { min = 10, max = 24 },
     Magazine = { min = 25, max = 35 },
     Range = { min = 23, max = 32 },
     Weight = { min = -7, max = -10 },
