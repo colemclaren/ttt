a70f958f19cf57c45f77ac2356f3c96c473961af
motato <cole@moat.gg>
2019-09-08 03:31:53 -0700

    Rename Vape in America's descriptions to free niggas (Posty's Assist)

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/other/vape_american2.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/other/vape_american2.lua
@@ -1,6 +1,6 @@
 ITEM.Name = "USA American Vape"
 ITEM.ID = 2580
-ITEM.Description = "We're free brooooo"
+ITEM.Description = "We're free nigga"
 ITEM.Rarity = 8
 ITEM.Image = "https://cdn.moat.gg/f/Rx0pUsUKgsDXYUzidrKNqe0L2yQS.png"
 ITEM.Collection = "Independence Collection"
