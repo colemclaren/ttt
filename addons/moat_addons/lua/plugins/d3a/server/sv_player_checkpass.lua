311963dfb2367aa4c9f1b975fa41eb39510ab4bb
motato <cole@moat.gg>
2019-09-14 19:07:43 -0700

    Restore level 10 req for TTC I think? Was setup to check MC #3.. Idk

--- addons/moat_addons/lua/plugins/d3a/server/sv_player_checkpass.lua
+++ addons/moat_addons/lua/plugins/d3a/server/sv_player_checkpass.lua
@@ -168,7 +168,7 @@ function D3A.Player.CheckPassword(SteamID, IP, sv_Pass, cl_Pass, Name)
 	-- Load or create player data
 	D3A.Player.Initialize(SteamID, SteamID32, Name, IP)
 
-	if (Server.IP and Server.IP == "208.103.169.43:27016") then
+	if (Server.IP and Server.IP == "208.103.169.43:27018") then
 		D3A.Player.CheckBetaAccess(SteamID, SteamID32)
 	end
 
