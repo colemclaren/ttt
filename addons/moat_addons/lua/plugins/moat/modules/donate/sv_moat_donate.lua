eb82cd3cf9c4894728672d03db71f52e68ce971a
motato <cole@moat.gg>
2019-10-03 06:43:27 -0700

    Pumpkin Crates are now available in Supporter Shop

--- addons/moat_addons/lua/plugins/moat/modules/donate/sv_moat_donate.lua
+++ addons/moat_addons/lua/plugins/moat/modules/donate/sv_moat_donate.lua
@@ -22,7 +22,7 @@ end
 function moat_DropIndependence(ply, amt)
 	for i = 1, amt do
         if (not IsValid(ply)) then return end
-        ply:m_DropInventoryItem("Independence Crate", "hide_chat_obtained", false, true)
+        ply:m_DropInventoryItem("Pumpkin Crate", "hide_chat_obtained", false, true)
     end
 end
 
