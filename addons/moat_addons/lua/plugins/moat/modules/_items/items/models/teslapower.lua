7ae50491efff08674cd7d697c23fa83e57657fa0
motato <cole@moat.gg>
2019-10-26 09:26:33 -0700

    Tesla power model is now temporarily another osama model until proper power replacement is found by a conscience scientist

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/models/teslapower.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/models/teslapower.lua
@@ -3,9 +3,9 @@ ITEM.ID = 562
 
 ITEM.Name = "Tesla Power Model"
 
-ITEM.Description = "If you aren't using a weapon with the Tesla Talent whilst wearing this skin, you're doing it wrong"
+ITEM.Description = "Temporarily osama model until proper power replacement is found by a conscience scientist" -- "If you aren't using a weapon with the Tesla Talent whilst wearing this skin, you're doing it wrong"
 
-ITEM.Model = "models/player/teslapower.mdl"
+ITEM.Model = "models/code_gs/osama/osamaplayer.mdl" -- "models/player/teslapower.mdl"
 
 ITEM.Rarity = 7
 
