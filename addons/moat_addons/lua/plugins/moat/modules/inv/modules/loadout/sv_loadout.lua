7778831a225cbf63f54f8c4320442858ee96b67e
motato <cole@moat.gg>
2019-11-05 04:48:12 -0800

    :+1:

--- addons/moat_addons/lua/plugins/moat/modules/inv/modules/loadout/sv_loadout.lua
+++ addons/moat_addons/lua/plugins/moat/modules/inv/modules/loadout/sv_loadout.lua
@@ -46,6 +46,7 @@ function MOAT_LOADOUT.ResetPowerupAbilities(ply)
     ply:SetJumpPower(160)
     ply.JumpHeight = 160
 	local max = 200
+	
 	if (max ~= 100) then
 		D3A.Chat.SendToPlayer2(ply,
 		Color(255, 0, 255), "[Head's up!] ",
