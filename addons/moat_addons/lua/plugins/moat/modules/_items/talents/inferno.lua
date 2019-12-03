fe8fe85e4144375f532fb27f7863dcb938bbeb2a
innocentmoat <cole@moat.gg>
2019-12-02 23:10:28 -0800

    Buffed the Inferno talent ignite length from 4-10 secs to 10-20 secs

--- addons/moat_addons/lua/plugins/moat/modules/_items/talents/inferno.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/talents/inferno.lua
@@ -9,7 +9,7 @@ TALENT.LevelRequired = { min = 25, max = 30 }
 
 TALENT.Modifications = {}
 TALENT.Modifications[1] = { min = 5, max = 10 }	-- Chance to ignite
-TALENT.Modifications[2] = { min = 4, max = 8 }	-- Ignite time
+TALENT.Modifications[2] = { min = 10, max = 20 }	-- Ignite time
 
 TALENT.Melee = true
 TALENT.NotUnique = true
