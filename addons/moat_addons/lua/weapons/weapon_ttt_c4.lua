9d254db3b77f918e91109cba5958779d23aa022b
innocentmoat <cole@moat.gg>
2020-02-18 23:11:06 -0800

    Added ttt_minecraftmotel_b2u to all minecraft map servers

--- addons/moat_addons/lua/weapons/weapon_ttt_c4.lua
+++ addons/moat_addons/lua/weapons/weapon_ttt_c4.lua
@@ -40,13 +40,13 @@ local c4disable = {
    ["STEAM_0:0:44950009"] = true
 }
 
-hook.Add("TTTBeginRound", "", function()
-   for _, ply in pairs(player.GetAll()) do
-      if (c4disable[ply:SteamID()]) then
-         weapons.GetStored[[weapon_ttt_c4]].CanBuy = {}
-      end
-   end
-end)
+-- hook.Add("TTTBeginRound", "", function()
+--    for _, ply in pairs(player.GetAll()) do
+--       if (c4disable[ply:SteamID()]) then
+--          weapons.GetStored[[weapon_ttt_c4]].CanBuy = {}
+--       end
+--    end
+-- end)
 
 SWEP.WeaponID = AMMO_C4
 
