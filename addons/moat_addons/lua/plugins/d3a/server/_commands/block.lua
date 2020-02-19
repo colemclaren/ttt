c293fae53c7f3fe469e88c1999f2aa92afa4f49c
innocentmoat <cole@moat.gg>
2020-02-19 13:41:21 -0800

    Cleaned up block and unblock from mod logs

--- addons/moat_addons/lua/plugins/d3a/server/_commands/block.lua
+++ addons/moat_addons/lua/plugins/d3a/server/_commands/block.lua
@@ -15,5 +15,5 @@ COMMAND.Run = function(pl, args, supp)
 	net.Send(pl)
 
 	D3A.Chat.SendToPlayer2(pl, moat_white," You have ", moat_cyan, "blocked ", moat_white, "communications with ", moat_green, supp[1]:Name(), moat_white, ".")
-	D3A.Commands.Discord("block", D3A.Commands.NameID(pl), IsValid(supp[1]) and supp[1]:NameID())
+	-- D3A.Commands.Discord("block", D3A.Commands.NameID(pl), IsValid(supp[1]) and supp[1]:NameID())
 end
