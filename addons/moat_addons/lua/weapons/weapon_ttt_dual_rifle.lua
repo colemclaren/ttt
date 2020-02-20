0ad7b2d8d0b759318f8f55b66a4fb8b2b085d7e0
innocentmoat <cole@moat.gg>
2020-02-20 13:25:06 -0800

    Added our new Rifle Lovers to the Valentine Collection

---
+++ addons/moat_addons/lua/weapons/weapon_ttt_dual_rifle.lua
@@ -0,0 +1,173 @@
+
+AddCSLuaFile()
+
+SWEP.HoldType = "duel"
+SWEP.PrintName = "Dual Rifles"
+
+SWEP.Slot = 2
+SWEP.Base = "weapon_ttt_dual_glock"
+
+if CLIENT then
+   SWEP.Slot      = 2
+
+   SWEP.Icon = "vgui/ttt/icon_scout"
+   SWEP.IconLetter = "n"
+end
+
+SWEP.Kind = WEAPON_HEAVY
+
+SWEP.Primary.Damage = 25
+SWEP.Primary.Delay = 1.5
+SWEP.Primary.Cone = 0.001
+SWEP.Primary.ClipSize = 15
+SWEP.Primary.ClipMax = 30
+SWEP.Primary.DefaultClip	= 15
+SWEP.Primary.Automatic		= true
+SWEP.Primary.Ammo			= "357"
+SWEP.Primary.NumShots = 2
+SWEP.AutoSpawnable      = false
+SWEP.AmmoEnt            = "item_ammo_357_ttt"
+SWEP.Primary.Recoil		= 14
+SWEP.Primary.Sound		= Sound("Weapon_Scout.Single")
+
+SWEP.UseHands			= false
+SWEP.ViewModelFlip		= false
+SWEP.ViewModelFOV		= 54
+SWEP.ViewModel          = Model("models/weapons/cstrike/c_snip_scout.mdl")
+SWEP.WorldModel         = Model("models/weapons/w_snip_scout.mdl")
+
+SWEP.HeadshotMultiplier = 5
+
+SWEP.DeploySpeed = 1.4
+SWEP.ReloadSpeed = 1
+SWEP.ReloadAnim = {
+	DefaultReload = {
+		Anim = "reload",
+		Time = 2.93333,
+	},
+}
+
+/*
+AddCSLuaFile()
+
+SWEP.HoldType           = "ar2"
+SWEP.PrintName          = "Rifle"
+
+if CLIENT then
+   SWEP.Slot               = 2
+   SWEP.Icon = "vgui/ttt/icon_scout"
+   SWEP.IconLetter = "n"
+end
+
+SWEP.Base               = "weapon_tttbase"
+SWEP.Spawnable = true
+
+SWEP.Kind = WEAPON_HEAVY
+SWEP.WeaponID = AMMO_RIFLE
+SWEP.ENUM = 3
+
+SWEP.Primary.Delay          = 1.5
+SWEP.Primary.Recoil         = 7
+SWEP.Primary.Automatic = true
+SWEP.Primary.Ammo = "357"
+SWEP.Primary.Damage = 50
+SWEP.Primary.Cone = 0.000001
+SWEP.Primary.ClipSize = 10
+SWEP.Primary.ClipMax = 20 -- keep mirrored to ammo
+SWEP.Primary.DefaultClip = 10
+
+SWEP.HeadshotMultiplier = 5
+
+SWEP.AutoSpawnable      = true
+SWEP.AmmoEnt = "item_ammo_357_ttt"
+
+SWEP.UseHands			= true
+SWEP.ViewModelFlip		= false
+SWEP.ViewModelFOV		= 54
+SWEP.ViewModel          = Model("models/weapons/cstrike/c_snip_scout.mdl")
+SWEP.WorldModel         = Model("models/weapons/w_snip_scout.mdl")
+
+SWEP.Primary.Sound = Sound("Weapon_Scout.Single")
+
+SWEP.Secondary.Sound = Sound("Default.Zoom")
+
+SWEP.IronSightsPos      = Vector( 5, -15, -2 )
+SWEP.IronSightsAng      = Vector( 2.6, 1.37, 3.5 )
+SWEP.Scope = true
+
+SWEP.DeploySpeed = 1.4
+SWEP.ReloadSpeed = 1
+SWEP.ReloadAnim = {
+	DefaultReload = {
+		Anim = "reload",
+		Time = 2.93333,
+	},
+}
+
+function SWEP:SetZoom(state)
+   if IsValid(self.Owner) and self.Owner:IsPlayer() then
+      if state then
+         self.Owner:SetFOV(20, 0.3)
+      else
+         self.Owner:SetFOV(0, 0.2)
+      end
+   end
+end
+
+if CLIENT then
+   local scope = surface.GetTextureID("sprites/scope")
+   function SWEP:DrawHUD()
+      if self:GetIronsights() then
+         surface.SetDrawColor( 0, 0, 0, 255 )
+         
+         local scrW = ScrW()
+         local scrH = ScrH()
+
+         local x = scrW / 2.0
+         local y = scrH / 2.0
+         local scope_size = scrH
+
+         -- crosshair
+         local gap = 80
+         local length = scope_size
+         surface.DrawLine( x - length, y, x - gap, y )
+         surface.DrawLine( x + length, y, x + gap, y )
+         surface.DrawLine( x, y - length, x, y - gap )
+         surface.DrawLine( x, y + length, x, y + gap )
+
+         gap = 0
+         length = 50
+         surface.DrawLine( x - length, y, x - gap, y )
+         surface.DrawLine( x + length, y, x + gap, y )
+         surface.DrawLine( x, y - length, x, y - gap )
+         surface.DrawLine( x, y + length, x, y + gap )
+
+
+         -- cover edges
+         local sh = scope_size / 2
+         local w = (x - sh) + 2
+         surface.DrawRect(0, 0, w, scope_size)
+         surface.DrawRect(x + sh - 2, 0, w, scope_size)
+         
+         -- cover gaps on top and bottom of screen
+         surface.DrawLine( 0, 0, scrW, 0 )
+         surface.DrawLine( 0, scrH - 1, scrW, scrH - 1 )
+
+         surface.SetDrawColor(255, 0, 0, 255)
+         surface.DrawLine(x, y, x + 1, y + 1)
+
+         -- scope
+         surface.SetTexture(scope)
+         surface.SetDrawColor(255, 255, 255, 255)
+
+         surface.DrawTexturedRectRotated(x, y, scope_size, scope_size, 0)
+      else
+         return self.BaseClass.DrawHUD(self)
+      end
+   end
+
+   function SWEP:AdjustMouseSensitivity()
+      return (self:GetIronsights() and 0.2) or nil
+   end
+end
+*/
