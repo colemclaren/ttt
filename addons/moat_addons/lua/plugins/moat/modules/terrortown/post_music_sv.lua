a6861a1ef1c206baeedfa47fddc72dcd96f0e684
innocentmoat <cole@moat.gg>
2020-02-22 21:27:00 -0800

    Added 20 more end round songs to our post round music playlist, thanks to Sparked!

--- addons/moat_addons/lua/plugins/moat/modules/terrortown/post_music_sv.lua
+++ addons/moat_addons/lua/plugins/moat/modules/terrortown/post_music_sv.lua
@@ -1,7 +1,7 @@
 util.AddNetworkString "Moat.PostRoundMusic"
 
 local moat_URL = "https://cdn.moat.gg/ttt/music/"
-local moat_Songs = 55
+local moat_Songs = 75
 
 -- local moat_URL = "https://cdn.moat.gg/ttt/music/hip-hop/"
 -- local moat_Songs = 131
