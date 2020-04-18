e61d747caf976fb52dcc58aa89b1147adbd460f6
colemclaren <cole@leafboat.com>
2020-04-17 20:59:58 -0700

    Added random vape to the shop as limited item for 4/20

--- addons/moat_addons/lua/plugins/moat/modules/_items/items/other/random_vape_limited.lua
+++ addons/moat_addons/lua/plugins/moat/modules/_items/items/other/random_vape_limited.lua
@@ -7,5 +7,5 @@ ITEM.Collection = "Shouldn't be in inventory"
 ITEM.WeaponClass = "weapon_ttt_unarmed"
 
 ITEM.Price = 64999
-ITEM.LimitedShop = 1555802194
+ITEM.LimitedShop = 1589785200
 ITEM.ShopDesc = "For a limited time, you can purchase a random vape!\nYou also have a chance to get a Mega Vape!"
