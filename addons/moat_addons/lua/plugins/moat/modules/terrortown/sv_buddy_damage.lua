d8a696ff7551ad90ac3811081ab01624ace86845
colemclaren <cole@leafboat.com>
2020-05-03 09:11:56 -0700

    Jester no longer takes burn, slash, vehicle, blast, physgun, and crush damage from Traitors

--- addons/moat_addons/lua/plugins/moat/modules/terrortown/sv_buddy_damage.lua
+++ addons/moat_addons/lua/plugins/moat/modules/terrortown/sv_buddy_damage.lua
@@ -18,7 +18,7 @@ hook("EntityTakeDamage", function(pl, dmg)
 
 
 	local att = dmg:GetAttacker()
-	if (IsValid(pl) and pl:IsPlayer() and pl:IsActiveTraitor() and IsValid(att) and att:IsPlayer() and att:GetRole() == ROLE_TRAITOR) then
+	if (IsValid(pl) and pl:IsPlayer() and (pl:IsActiveRole(ROLE_JESTER) or pl:IsActiveTraitor()) and IsValid(att) and att:IsPlayer() and att:GetRole() == ROLE_TRAITOR) then
 		if (pl == att and dmg:GetDamageType() == DMG_BLAST) then
 			return
 		end
