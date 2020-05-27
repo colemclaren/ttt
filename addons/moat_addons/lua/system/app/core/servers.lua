fcaad78c47ad6073b39bad23aff83d36922150b2
colemclaren <cole@leafboat.com>
2020-05-27 03:08:04 -0700

    : Redeploy

--- addons/moat_addons/lua/system/app/core/servers.lua
+++ addons/moat_addons/lua/system/app/core/servers.lua
@@ -7,133 +7,133 @@
 Servers.Setup("Moat TTT", ".moat.gg")
 
 Servers "208.103.169.28:27015"
-	:SetName "Beta I"
+	:SetName "Beta #1"
 	:SetID "ttt"
 
 Servers "208.103.169.30:27015"
-	:SetName "Beta III"
+	:SetName "Beta #2"
 	:SetID "ttt2"
 
 Servers "208.103.169.31:27015"
-	:SetName "Beta V"
+	:SetName "Beta #3"
 	:SetID "ttt3"
 
 Servers "208.103.169.31:27017"
-	:SetName "Beta VI"
+	:SetName "Beta #4"
 	:SetID "ttt4"
 
 Servers "208.103.169.43:27015"
-	:SetName "Beta VIII"
+	:SetName "Beta #5"
 	:SetID "ttt5"
 
 Servers "208.103.169.43:27017"
-	:SetName "Beta IX"
+	:SetName "Beta #6"
 	:SetID "ttt6"
 
 Servers "208.103.169.43:27018"
-	:SetName "Beta X"
+	:SetName "Beta #7"
 	:SetID "ttt7"
 
 Servers "208.103.169.43:27019"
-	:SetName "Beta XV"
+	:SetName "Beta #8"
 	:SetID "ttt8"
 
 Servers "208.103.169.43:27020"
-	:SetName "Beta XVII"
+	:SetName "Beta #9"
 	:SetID "ttt9"
 
 Servers "208.103.169.54:27015"
-	:SetName "Beta XVIII"
+	:SetName "Beta #10"
 	:SetID "ttt10"
 
 Servers "208.103.169.54:27017"
-	:SetName "Beta XXIII"
+	:SetName "Beta #11"
 	:SetID "ttt11"
 
 Servers "208.103.169.54:27018"
-	:SetName "Beta XXX"
+	:SetName "Beta #12"
 	:SetID "ttt12"
 
 Servers "208.103.169.205:27015"
-	:SetName "Beta EU I"
+	:SetName "Beta EU #1"
 	:SetID "ttt-eu1"
 
 Servers "208.103.169.205:27017"
-	:SetName "Beta EU III"
+	:SetName "Beta EU #2"
 	:SetID "ttt-eu2"
 
 Servers "208.103.169.205:27018"
-	:SetName "Beta EU V"
+	:SetName "Beta EU #3"
 	:SetID "ttt-eu3"
 
 Servers "208.103.169.205:27020"
-	:SetName "Beta EU VI"
+	:SetName "Beta EU #4"
 	:SetID "ttt-eu4"
 
 Servers "208.103.169.29:27015"
-	:SetName "Beta Minecraft I"
+	:SetName "Beta Minecraft #1"
 	:SetID "ttt-mc"
 
 Servers "208.103.169.31:27016"
-	:SetName "Beta Minecraft III"
+	:SetName "Beta Minecraft #2"
 	:SetID "ttt-mc2"
 
 Servers "208.103.169.43:27016"
-	:SetName "Beta Minecraft V"
+	:SetName "Beta Minecraft #3"
 	:SetID "ttt-mc3"
 
 Servers "208.103.169.54:27016"
-	:SetName "Beta Minecraft VI"
+	:SetName "Beta Minecraft #4"
 	:SetID "ttt-mc4"
 
 Servers "208.103.169.54:27019"
-	:SetName "Beta Minecraft VIII"
+	:SetName "Beta Minecraft #5"
 	:SetID "ttt-mc5"
 
 Servers "208.103.169.31:27018"
-	:SetName "Beta Minecraft IX"
+	:SetName "Beta Minecraft #6"
 	:SetID "ttt-mc6"
 
 Servers "208.103.169.31:27019"
-	:SetName "Beta Minecraft X"
+	:SetName "Beta Minecraft #7"
 	:SetID "ttt-mc7"
 
 Servers "208.103.169.205:27016"
-	:SetName "Beta Minecraft EU I"
+	:SetName "Beta Minecraft EU #1"
 	:SetID "ttt-eumc1"
 
 Servers "208.103.169.205:27019"
-	:SetName "Beta Minecraft EU III"
+	:SetName "Beta Minecraft EU #2"
 	:SetID "ttt-eumc2"
 
 Servers "208.103.169.54:27020"
-	:SetName "TTC Beta I"
+	:SetName "TTC Beta #1"
 	:SetID "beta"
 
 Servers "208.103.169.192:27015"
-	:SetName "TTC Beta III"
-	:SetID "ttc3"
+	:SetName "TTC Beta #2"
+	:SetID "ttc2"
 
 Servers "208.103.169.192:27016"
-	:SetName "TTC Beta V"
-	:SetID "ttc5"
+	:SetName "TTC Beta #3"
+	:SetID "ttc3"
 
 Servers "208.103.169.192:27017"
-	:SetName "TTC Beta VI"
-	:SetID "ttc6"
+	:SetName "TTC Beta #4"
+	:SetID "ttc4"
 
 Servers "208.103.169.192:27018"
-	:SetName "TTC Beta Minecraft I"
+	:SetName "TTC Beta Minecraft #1"
 	:SetID "ttc-mc"
 
 Servers "208.103.169.192:27019"
-	:SetName "TTC Beta Minecraft III"
-	:SetID "ttc-mc3"
+	:SetName "TTC Beta Minecraft #2"
+	:SetID "ttc-mc2"
 
 Servers "208.103.169.192:27020"
-	:SetName "TTC Beta Minecraft V"
-	:SetID "ttc-mc5"
+	:SetName "TTC Beta Minecraft #3"
+	:SetID "ttc-mc3"
 
 Servers "208.103.169.192:27021"
-	:SetName "TTC Beta Minecraft VI"
-	:SetID "ttc-mc6"
+	:SetName "TTC Beta Minecraft #4"
+	:SetID "ttc-mc4"
