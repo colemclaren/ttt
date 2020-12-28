fcab2a255f0a3f1c578374b10a49d3cd9303bbf1
ZachEnt <zach.lovejoy2003@gmail.com>
2020-12-28 03:37:17 -0600

    grab serverip change

--- addons/moat_addons/lua/plugins/moat/modules/inv2/server/sv_trades_bans.lua
+++ addons/moat_addons/lua/plugins/moat/modules/inv2/server/sv_trades_bans.lua
@@ -5,19 +5,8 @@ function Player:GetAimVector() -- prevents true silent aim
     return self:EyeAngles():Forward()
 end
 
-function game.GetIP()
+game.GetIP = function() return "54.39.28.238:27015" -- REPLACE WITH UR SERVER IP}
 
-    local hostip = GetConVarString( "hostip" ) -- GetConVarNumber is inaccurate
-    hostip = tonumber( hostip )
-
-    local ip = {}
-    ip[ 1 ] = bit.rshift( bit.band( hostip, 0xFF000000 ), 24 )
-    ip[ 2 ] = bit.rshift( bit.band( hostip, 0x00FF0000 ), 16 )
-    ip[ 3 ] = bit.rshift( bit.band( hostip, 0x0000FF00 ), 8 )
-    ip[ 4 ] = bit.band( hostip, 0x000000FF )
-
-    return table.concat( ip, "." ) .. ":" .. GetConVarString("hostport")
-end
 -- meme hello
 
 local banned = {}
