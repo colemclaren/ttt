b1d5a372a657f1d4e03e329c7da40a37a0f1fe1c
Aleksandrs Filipovskis <tochnonement@gmail.com>
2021-05-09 18:51:47 +0300

    Fixed table creation sequence
    mg_itemnames tries to create constraint to non yet created table

--- db.sql
+++ db.sql
@@ -445,6 +445,18 @@ CREATE TABLE IF NOT EXISTS `lola_error_reports` (
 	PRIMARY KEY (`id`)
 );
 
+CREATE TABLE IF NOT EXISTS `mg_items` (
+	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+	`itemid` int(10) unsigned NOT NULL,
+	`ownerid` bigint(20) unsigned NOT NULL,
+	`slotid` int(10) DEFAULT NULL,
+	`classname` varchar(32) DEFAULT NULL,
+	`createdat` timestamp DEFAULT CURRENT_TIMESTAMP,
+	`updatedat` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	PRIMARY KEY (`id`),
+	KEY `ownerid` (`ownerid`)
+);
+
 CREATE TABLE IF NOT EXISTS `mg_itemnames` (
 	`weaponid` int(10) unsigned NOT NULL,
 	`nickname` varchar(32) NOT NULL,
@@ -461,18 +473,6 @@ CREATE TABLE IF NOT EXISTS `mg_itempaints` (
 	PRIMARY KEY (`weaponid`,`type`),
 	KEY `weaponid` (`weaponid`),
 	CONSTRAINT `fk_mg_itempaints_mg_items` FOREIGN KEY (`weaponid`) REFERENCES `mg_items` (`id`) ON DELETE CASCADE
-);
-
-CREATE TABLE IF NOT EXISTS `mg_items` (
-	`id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-	`itemid` int(10) unsigned NOT NULL,
-	`ownerid` bigint(20) unsigned NOT NULL,
-	`slotid` int(10) DEFAULT NULL,
-	`classname` varchar(32) DEFAULT NULL,
-	`createdat` timestamp DEFAULT CURRENT_TIMESTAMP,
-	`updatedat` timestamp DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-	PRIMARY KEY (`id`),
-	KEY `ownerid` (`ownerid`)
 );
 
 CREATE TABLE IF NOT EXISTS `mg_itemstats` (
